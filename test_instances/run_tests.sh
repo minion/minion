c7f5cedfa598fafdeb16d51d6143c07c8186fa2a
Christopher Jefferson <chris@bubblescope.net>
2008-01-16 23:41:03 +0000

    Move to trunk
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@444 477b24dd-e91e-0410-aa49-980f3df0729b

---
+++ test_instances/run_tests.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+j=0
+for i in *.minion; do
+j=$(($j + 1))
+if grep "#FAIL" $i > /dev/null;
+then
+  if $1 -test $2 $i &> /dev/null;
+    then echo $i should have failed;
+  else
+    pass=$(($pass + 1));
+  fi
+else
+  if $1 -test $2 $i &> /dev/null;
+    then pass=$(($pass + 1));
+  else 
+    echo Fail $i;
+  fi
+fi
+done
+echo $pass of $j tests successful.
+echo Doing randomised tests.
+j=0
+pass=0
+for i in `grep -l "#TEST NOSOLS\|#TEST SOLCOUNT" *.minion`; do
+  LOOP=0
+  if grep "#FAIL" $i &> /dev/null;
+  then
+    echo -n 
+  else
+    while [ $LOOP -lt 10 ]; do
+      j=$(($j + 1));
+      if $1 -test $2 -randomiseorder $i &> test_output
+      then
+        pass=$(($pass + 1));
+      else
+        echo Fail $i. Consult test_output;
+        exit;
+      fi
+      let LOOP=LOOP+1
+    done
+    fi
+  echo -n .
+done
+echo $pass of $j randomised tests successful.
+rm test_output
