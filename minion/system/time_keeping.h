2b8bdad11a2d6750318faf74243e8080c23c4456
Christopher Jefferson <chris@bubblescope.net>
2008-01-16 23:44:48 +0000

    Fix depricated functions
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@576 477b24dd-e91e-0410-aa49-980f3df0729b

--- minion/system/time_keeping.h
+++ minion/system/time_keeping.h
@@ -38,18 +38,18 @@ inline void start_clock()
 inline bool check_timeout(unsigned seconds)
 { return (clock() - _internal_start_time) >= (seconds * CLOCKS_PER_SEC); }
 
-inline void print_timestep_without_reset(char* time_name)
+inline void print_timestep_without_reset(const char* time_name)
 {
   cout << time_name << (clock() - _last_check_time) / (1.0 * CLOCKS_PER_SEC) << endl;
 } 
-inline void print_timestep(char* time_name)
+inline void print_timestep(const char* time_name)
 {
   clock_t current_time = clock();
   cout << time_name << (current_time - _last_check_time) / (1.0 * CLOCKS_PER_SEC) << endl;
   _last_check_time = current_time;
 }
 
-inline void print_finaltimestep(char* time_name)
+inline void print_finaltimestep(const char* time_name)
 {
   print_timestep(time_name);
   cout << "Total Time: " << (_last_check_time - _internal_start_time) / (1.0 * CLOCKS_PER_SEC) << endl;
