9982ca89910431710fb04f188960ce22341a5297
Christopher Jefferson <chris@bubblescope.net>
2008-01-16 23:55:39 +0000

    Add extra flags
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@699 477b24dd-e91e-0410-aa49-980f3df0729b

--- release-scripts/make-release-intel-mac.sh
+++ release-scripts/make-release-intel-mac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ..
-./build-all.sh MYFLAGS=" -fast -march=pentium-m -mdynamic-no-pic "
+./build-all.sh MYFLAGS=" -fast -march=pentium-m -mdynamic-no-pic -fomit-frame-pointer"
 cd release-scripts
 ./make-release.sh intel-mac
--- release-scripts/make-release-ppc-mac.sh
+++ release-scripts/make-release-ppc-mac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ..
-./build-all.sh MYFLAGS=" -fast -arch ppc -mcpu=G4 -mdynamic-no-pic "
+./build-all.sh MYFLAGS=" -fast -arch ppc -mcpu=G4 -mdynamic-no-pic -fomit-frame-pointer"
 cd release-scripts
 ./make-release.sh ppc-mac
