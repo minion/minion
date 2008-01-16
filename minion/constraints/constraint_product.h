560f7d439bf9d5e7d3e34dc853b9051059e76e4f
Christopher Jefferson <chris@bubblescope.net>
2008-01-16 23:43:56 +0000

    Fix product problems
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@545 477b24dd-e91e-0410-aa49-980f3df0729b

--- minion/constraints/constraint_product.h
+++ minion/constraints/constraint_product.h
@@ -43,44 +43,91 @@ struct ProductConstraint : public Constraint
 	return t;
   }
   
-  int max(int min1, int max1, int min2, int max2)
+  int mult_max(int min1, int max1, int min2, int max2)
   { return mymax(mymax(min1*min2, min1*max2),mymax(max1*min2, max1*max2)); }
   
-  int min(int min1, int max1, int min2, int max2)
+  int mult_min(int min1, int max1, int min2, int max2)
   { return mymin(mymin(min1*min2, min1*max2),mymin(max1*min2, max1*max2)); }
+  
+  int round_up_div(int x, int y)
+  {
+	if(y == 0)
+	  return 0;
+	div_t division = div(x,y);
+	int return_val = division.quot;
+	if(division.rem != 0)
+	  return_val++;
+	return return_val;
+  }
+  
+  int round_down_div(int x, int y)
+  { 
+	if(y == 0)
+	  return INT_MAX;
+	return x / y; 
+  }
   
   PROPAGATE_FUNCTION(int, DomainDelta)
   {
-	var3.setMax(max(var1.getMin(), var1.getMax(), var2.getMin(), var2.getMax()));
-	var3.setMin(min(var1.getMin(), var1.getMax(), var2.getMin(), var2.getMax()));
+	int var1_min = var1.getMin();
+	int var1_max = var1.getMax();
+	int var2_min = var2.getMin();
+	int var2_max = var2.getMax();
+	int var3_min = var3.getMin();
+	int var3_max = var3.getMax();
 	
-	if(var1.isAssigned())
+	if((var1_min >= 0) && (var2_min >= 0))
 	{
-	  int val1 = var1.getAssignedValue();
-	  if(val1 > 0)
-	  { 
-		var3.setMin(var2.getMin() * val1);
-		var3.setMax(var2.getMax() * val1);
-	  }
-	  else
-	  {
-		var3.setMin(var2.getMax() * val1);
-		var3.setMax(var2.getMin() * val1);
-	  }
+	  // We don't have to deal with negative numbers. yay!
+	  
+	  var3_min = max(var3_min, var1_min * var2_min);
+	  var3_max = min(var3_max, var1_max * var2_max);
+	  
+	  var1_min = max(var1_min, round_up_div(var3_min, var2_max));
+	  var1_max = min(var1_max, round_down_div(var3_max, var2_min));
+	  
+	  var2_min = max(var2_min, round_up_div(var3_min, var1_max));
+	  var2_max = min(var2_max, round_down_div(var3_max, var1_min));
+	  
+	  var1.setMin(var1_min);
+	  var1.setMax(var1_max);
+	  var2.setMin(var2_min);
+	  var2.setMax(var2_max);
+	  var3.setMin(var3_min);
+	  var3.setMax(var3_max);
 	}
-	
-	if(var2.isAssigned())
+    else
 	{
-	  int val2 = var2.getAssignedValue();
-	  if(val2 > 0)
-	  { 
-		var3.setMin(var1.getMin() * val2);
-		var3.setMax(var1.getMax() * val2);
+	  var3.setMax(mult_max(var1_min, var1_max, var2_min, var2_max));
+	  var3.setMin(mult_min(var1_min, var1_max, var2_min, var2_max));
+	  if(var1.isAssigned())
+  	  {
+	    int val1 = var1.getAssignedValue();
+	    if(val1 > 0)
+	    { 
+		  var3.setMin(var2.getMin() * val1);
+		  var3.setMax(var2.getMax() * val1);
+		}
+		else
+		{
+		  var3.setMin(var2.getMax() * val1);
+		  var3.setMax(var2.getMin() * val1);
+		}
 	  }
-	  else
+	  
+	  if(var2.isAssigned())
 	  {
-		var3.setMin(var1.getMax() * val2);
-		var3.setMax(var1.getMin() * val2);
+		int val2 = var2.getAssignedValue();
+		if(val2 > 0)
+		{ 
+		  var3.setMin(var1.getMin() * val2);
+		  var3.setMax(var1.getMax() * val2);
+		}
+		else
+		{
+		  var3.setMin(var1.getMax() * val2);
+		  var3.setMax(var1.getMin() * val2);
+		}
 	  }
 	}
   }
