37960dc0cdfd0984b66af402d6eeefd340d1e75b
Christopher Jefferson <chris@bubblescope.net>
2008-01-16 23:53:00 +0000

    Fix out-of-date build file
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@630 477b24dd-e91e-0410-aa49-980f3df0729b

--- make_table_minions.sh
+++ make_table_minions.sh
@@ -1,12 +1,12 @@
 nice make minion -j 2
 cp bin/minion bin/min-old
-rm bin/objdir-minion/BuildDynamicConstraint_GACTable.o
-make minion MYFLAGS="-DNIGHTINGALE"
+rm bin/objdir-minion/build_constraints/*TABLE*
+make minion MYFLAGS="-DNIGHTINGALE" -j 2
 cp bin/minion bin/min-night
-rm bin/objdir-minion/BuildDynamicConstraint_GACTable.o
-make minion MYFLAGS="-DTRIES"
+rm bin/objdir-minion/build_constraints/*TABLE*
+make minion MYFLAGS="-DTRIES" -j 2
 cp bin/minion bin/min-tries
-#rm bin/objdir-minion/BuildDynamicConstraint_GACTable.o
+rm bin/objdir-minion/build_constraints/*TABLE*
 #make minion MYFLAGS="-DREGINLHOMME"
 #cp bin/minion bin/min-regin
 
