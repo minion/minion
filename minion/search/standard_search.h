e9b628620a9bed1a23afac692bc1aa0e12980e0d
Christopher Jefferson <chris@bubblescope.net>
2008-01-16 23:44:27 +0000

    Add SDF
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@564 477b24dd-e91e-0410-aa49-980f3df0729b

--- minion/search/standard_search.h
+++ minion/search/standard_search.h
@@ -6,24 +6,6 @@
    $Id$
 */
 
-/* Minion
-* Copyright (C) 2006
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
-
 namespace Controller
 {
 
@@ -36,6 +18,7 @@ namespace Controller
 
   VARDEF_ASSIGN(BOOL _find_one_sol,true);
   VARDEF_ASSIGN(BOOL print_solution, true);
+  VARDEF_ASSIGN(BOOL use_sdf, false);
   
   inline void find_all_solutions()
   { _find_one_sol = false; }
