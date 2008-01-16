723a224f1b64b1964974e1e521bfcc8870c2bdd2
Christopher Jefferson <chris@bubblescope.net>
2008-01-16 23:43:34 +0000

    New test script
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@529 477b24dd-e91e-0410-aa49-980f3df0729b

--- test_instances/run_tests.sh
+++ test_instances/run_tests.sh
@@ -1,58 +1,100 @@
 #!/bin/bash
+stripsols= 
 j=0
 reify=0
 pass=0
 expectedfail=0
+unexpectedpass=0
+
 for i in *.minion; do
   echo -n .
   j=$(($j + 1))
 
+  # This gives the value that the program should return.
   if grep -q "#FAIL" $i;
-    then fail=1
-    else fail=0
-  fi
-
-  if grep -q "#TEST SOLCOUNT" $i;
-  then
-    sols=`$1 -findallsols $2 $i | ../mini-scripts/solutions.sh`
-    testsols=`grep "#TEST SOLCOUNT" $i | awk '{print $3}'`
-	echo $i . $sols . $testsols
-	if [[ "$sols" != "$testsols" ]]; then
-	  echo Got $testsols instead of $sols solutions in $2
-	else
-	  pass=$(($pass + 1))
-	fi
+  then 
+    correctoutput=0
+  else
+    correctoutput=1  
   fi
   
+  if grep -q "#BUG" $i;
+  then 
+    bug=1
+  else 
+    bug=0
+  fi
+  
+  if grep -q "#TEST SOLCOUNT" $i;
+  then
+    numsols=`$1 -findallsols $2 $i | ../mini-scripts/solutions.sh`
+    testnumsols=`grep "#TEST SOLCOUNT" $i 2>nul | awk '{print $3}' | tr -d '\015' `
+	if [[ "$numsols" != "$testnumsols" ]]; then
+	  testpass=0
+	  errormess="Got '${testnumsols}' instead of '{$numsols}' solutions in $i"
+	else
+	  testpass=1
+	fi
+  else
+    if grep -q "#TEST CHECKONESOL" $i; then
+      sol=`$1 $2 $i | ../mini-scripts/print_sol.sh`
+      testsol=`grep "#TEST CHECKONESOL" $i | awk '{$1 = ""; $2 = ""; print }' | tr -d '\015' `
+      sol=`echo $sol | sed -e "s/^ \{1,\}//;s/ \{1,\}$//;s/ \{1,\}/,/g;"`
+      testsol=`echo $testsol | sed -e "s/^ \{1,\}//;s/ \{1,\}$//;s/ \{1,\}/,/g;"`
+      if [[ "$sol" = "$testsol" ]]; then
+        testpass=1
+      else
+        testpass=0
+        errormess="Got '${sol}' instead of '${testsol}' as solution in $i"
+      fi
+    else
+      echo Test $i is not well-formed.
+      exit 0
+    fi  
+  fi
+  
+  if [ "$bug" = "0" ]; then
+    if [ "$testpass" = "$correctoutput" ]; then
+      pass=$(($pass + 1))
+    else
+      if [ "$correctoutput" = "0" ]; then
+        echo Expected $i to fail.
+      else
+        echo $errormess
+      fi
+    fi
+  else
+    if [ "$testpass" = "$correctoutput" ]; then
+      echo $i passed, but is supposed to be buggy!
+      unexpectedpass=$(($unexpectedpass + 1))
+    else
+      expectedfail=$(($expectedfail + 1))
+    fi
+  fi  
 done
+
 echo
-echo "$pass of $j tests successful."
-echo $(($j - $pass - $reify - $expectedfail)) tests failed due to unexpected errors.
-echo $reify tests failed due to unimplemented reification.
-if [[ ! ($expectedfail -eq 0) ]]
-then 
-   echo $expectedfail tests failed due to other expected errors,  e.g. known bugs
-fi
-
-
+echo $pass of $j tests successful.
+echo $(($j - $pass - $expectedfail)) tests failed due to unexpected errors.
+echo $expectedfail tests failed due to expected errors.
+echo $unexpectedpass tests passed unexpectedly.
 
 echo Doing randomised tests.
 j=0
 pass=0
-for i in `grep -l "#TEST NOSOLS\|#TEST SOLCOUNT" *.minion`; do
+for i in `grep -l "#TEST SOLCOUNT" *.minion`; do
   LOOP=0
-  if grep "#FAIL" $i &> /dev/null;
+  if grep "#FAIL\|#BUG" $i &> /dev/null;
   then
     echo -n 
   else
     while [ $LOOP -lt 10 ]; do
       j=$(($j + 1));
-      if $1 -test $2 -randomiseorder $i &> test_output
+      if $1 $2 -randomiseorder $i &> /dev/null
       then
         pass=$(($pass + 1));
       else
-        echo Fail $i. Consult test_output;
-        exit;
+        echo Fail $i;
       fi
       let LOOP=LOOP+1
     done
