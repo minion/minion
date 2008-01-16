c90f509128f09c847e9cd78f66bf00e295427897
Christopher Jefferson <chris@bubblescope.net>
2008-01-16 23:55:18 +0000

    Seperate out random tests
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@687 477b24dd-e91e-0410-aa49-980f3df0729b

---
+++ test_instances/do_random_tests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+if [ $# -lt 1 ]; then
+  echo Must give a minion binary to test.
+  echo Likely values are ../bin/minion or ../bin/minion-debug
+  exit 0
+fi
+
+if [ ! -x $1 ]
+then
+  echo $1 either doesn\'t exist, or isn\'t executable.
+  exit 0
+fi
+
+exec=$1
+#Remove exec from $*, so it only contains parameters
+shift
+echo Random testing $exec with options .$*.
+
+j=0
+pass=0
+for i in `grep -l "#TEST SOLCOUNT" *.minion`; do
+  LOOP=0
+  if grep "#FAIL\|#BUG" $i &> /dev/null;
+  then
+    echo -n 
+  else
+    while [ $LOOP -lt 10 ]; do
+      j=$(($j + 1));
+      if $exec $* -randomiseorder $i &> /dev/null
+      then
+        pass=$(($pass + 1));
+      else
+        echo Fail $i;
+      fi
+      let LOOP=LOOP+1
+    done
+    fi
+  echo -n .
+done
+echo
+echo $pass of $j randomised tests successful.
--- test_instances/run_tests.sh
+++ test_instances/run_tests.sh
@@ -100,28 +100,4 @@ echo $(($j - $pass - $expectedfail)) tests failed due to unexpected errors.
 echo $expectedfail tests failed due to expected errors.
 echo $unexpectedpass tests passed unexpectedly.
 
-echo Doing randomised tests.
-j=0
-pass=0
-for i in `grep -l "#TEST SOLCOUNT" *.minion`; do
-  LOOP=0
-  if grep "#FAIL\|#BUG" $i &> /dev/null;
-  then
-    echo -n 
-  else
-    while [ $LOOP -lt 10 ]; do
-      j=$(($j + 1));
-      if $exec $* -randomiseorder $i &> /dev/null
-      then
-        pass=$(($pass + 1));
-      else
-        echo Fail $i;
-      fi
-      let LOOP=LOOP+1
-    done
-    fi
-  echo -n .
-done
-echo
-echo $pass of $j randomised tests successful.
-
+./do_random_tests $exec $*
