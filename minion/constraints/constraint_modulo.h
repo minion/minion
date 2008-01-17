77c5ff60bed0df524b07e8855d484896bfc86ef2
Christopher Jefferson <chris@bubblescope.net>
2008-01-17 00:30:09 +0000

    Simple ternary modulo constraint. Needs to be converted to use watched literals in the future.
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@804 477b24dd-e91e-0410-aa49-980f3df0729b

--- minion/constraints/constraint_modulo.h
+++ minion/constraints/constraint_modulo.h
@@ -24,12 +24,7 @@
 * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-
-#include <math.h>
-
-#define LRINT(x) static_cast<long>(x + 0.5)
-
-/// var1 ^ var2 = var3
+/// var1 % var2 = var3
 template<typename VarRef1, typename VarRef2, typename VarRef3>
 struct ModConstraint : public Constraint
 {
@@ -44,10 +39,10 @@ struct ModConstraint : public Constraint
 	var1(_var1), var2(_var2), var3(_var3)
   {
   
-	  if(var1.getInitialMin() < 0 || var2.getInitialMin() < 0 ||
+	  if(var1.getInitialMin() < 0 || var2.getInitialMin() < 1 ||
 		 var3.getInitialMin() < 0)
 	  { 
-		cerr << "The 'pow' constraint only supports positive numbers at present.";
+		cerr << "The 'modulo' constraint only supports nonnegative numbers, and positive bases, at present.";
 		exit(1);
 	  }
   }
@@ -67,17 +62,145 @@ struct ModConstraint : public Constraint
   
   PROPAGATE_FUNCTION(int flag, DomainDelta)
   {
-	PROP_INFO_ADDONE(Pow);
+	PROP_INFO_ADDONE(Mod);
+    // aiming at bounds(D)-consistency. 
+    // Not achieved because we don't trigger on the supporting values, only bounds.
+    
+    // var1 upperbound
+    DomainInt var1val=var1.getMax();
+    while(!testsupport_var1(var1val))
+    {
+        // While no support for upperbound, reduce upperbound
+        var1.removeFromDomain(var1val);  // Is this the right function for pruning the upperbound?
+        var1val=var1.getMax();
+        if(Controller::failed) return;  // Does not work with setjmp!
+    }
+    
+    var1val=var1.getMin();
+    while(!testsupport_var1(var1val))
+    {
+        var1.removeFromDomain(var1val);
+        var1val=var1.getMin();
+        if(Controller::failed) return;
+    }
+    
+    DomainInt var2val=var2.getMax();
+    while(!testsupport_var2(var2val))
+    {
+        // While no support for upperbound, reduce upperbound
+        var2.removeFromDomain(var2val);  // Is this the right function for pruning the upperbound?
+        var2val=var2.getMax();
+        if(Controller::failed) return;  // Does not work with setjmp!
+    }
+    
+    var2val=var2.getMin();
+    while(!testsupport_var2(var2val))
+    {
+        var2.removeFromDomain(var2val);
+        var2val=var2.getMin();
+        if(Controller::failed) return;
+    }
+    
+    DomainInt var3val=var3.getMax();
+    while(!testsupport_var3(var3val))
+    {
+        // While no support for upperbound, reduce upperbound
+        var3.removeFromDomain(var3val);  // Is this the right function for pruning the upperbound?
+        var3val=var3.getMax();
+        if(Controller::failed) return;  // Does not work with setjmp!
+    }
+    
+    var3val=var3.getMin();
+    while(!testsupport_var3(var3val))
+    {
+        var3.removeFromDomain(var3val);
+        var3val=var3.getMin();
+        if(Controller::failed) return;
+    }
+    
+  }
+  
+  // next step is to modify the following three functions to store the supporting tuple,
+  // then watch the literals in that tuple. 
+  
+  inline BOOL testsupport_var1(DomainInt var1val)
+  {
+    // If this returns true var1val would be supported by
+    // var2=var2.getMax , var3=var1val
+    
+    if(var2.getMax()>var1val && var3.inDomain(var1val))
+    {
+        return true;
+    }
+    for(DomainInt var2val=var2.getMin(); var2val<=var2.getMax() && var2val<=var1val; ++var2val)
+    {
+        if(var2.inDomain(var2val) && var3.inDomain(var1val % var2val))
+        {
+            return true;
+        }
+    }
+    return false;
+  }
+  
+  inline BOOL testsupport_var2(DomainInt var2val)
+  {
+      // var1 % var2 = var3
+      for(DomainInt var1val=var1.getMin(); var1val<=var1.getMax(); ++var1val)
+      {
+          if(var1.inDomain(var1val) && var3.inDomain(var1val % var2val))
+          {
+              return true;
+          }
+      }
+      return false;
+  }
+  
+  inline BOOL testsupport_var3(DomainInt var3val)
+  {
+      // Iterate through var2 then
+      // through the values of var1 which are congruent with var3val.
+      DomainInt var2val=var2.getMin();
+      
+      if(var2val<=var3val) var2val=var3val+1;  // var3val<var2val always.
+        for(; var2val<=var2.getMax(); ++var2val)
+        {
+            if(var2.inDomain(var2val))
+            {
+                DomainInt var1val=var1.getMin();
+                // need to jump to the next value which is congruent with var3 (mod var2)
+                DomainInt mod=var1val%var2val;
+                if(mod<var3val)
+                {
+                    var1val+=(var3val-mod);
+                    if((var1val%var2val) != var3val) {cerr << "What on earth?1" << endl;}
+                }
+                if(mod>var3val)
+                {
+                    var1val+=(var3val-mod)+var2val;
+                    if((var1val%var2val) != var3val) {cerr << "What on earth?2" << endl;}
+                }
+                if((var1val%var2val) != var3val) {cerr << var1val <<","<<var2val<<","<<var3val << endl;}
+                for(; var1val<=var1.getMax(); var1val+=var2val) 
+                    // Skips values which are not congruent.
+                {
+                    if(var1.inDomain(var1val) && (var1val % var2val)==var3val)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
   }
   
   virtual void full_propogate()
   { 
 	propogate(1,0); 
-    propogate(2,0);
+    /*propogate(2,0);
     propogate(3,0);
     propogate(-1,0);
     propogate(-2,0);
-    propogate(-3,0);
+    propogate(-3,0);*/
   }
   
   virtual BOOL check_assignment(vector<DomainInt> v)
