6c4c008bb98bc9e18728bf934dae6015268feb56
Christopher Jefferson <chris@bubblescope.net>
2008-01-17 00:44:30 +0000

    Update release scripts
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@1027 477b24dd-e91e-0410-aa49-980f3df0729b

--- release-scripts/make-release.sh
+++ release-scripts/make-release.sh
@@ -10,6 +10,10 @@ cp LICENSE.txt minion-release-$1
 cp -r test_instances minion-release-$1
 cp -r benchmarks minion-release-$1
 cp Changelog minion-release-$1
+cp translator/translator.jar minion-release-$1/eprime-translator.jar
+cp translator/README minion-release-$1/README.translator
+cp -r translator/examples minion-release-$1/translator-examples
+cp -r translator/doc minion-release-$1/translator-doc
 find ./minion-release-$1 -type d -name '.svn' -print0 | xargs -0 rm -rdf
 rm minion-$1.zip
 zip -r -9 minion-$1.zip minion-release-$1
