64c9572f45da6903ae56a844c9b994442f214ccb
Christopher Jefferson <chris@bubblescope.net>
2008-01-17 00:26:04 +0000

    Final changes before 0.4.1
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@725 477b24dd-e91e-0410-aa49-980f3df0729b

--- release-scripts/make-release.sh
+++ release-scripts/make-release.sh
@@ -9,6 +9,7 @@ cp docs/manual.pdf minion-release-$1
 cp LICENSE.txt minion-release-$1
 cp -r test_instances minion-release-$1
 cp -r benchmarks minion-release-$1
+cp Changelog minion-release-$1
 find ./minion-release-$1 -type d -name '.svn' -print0 | xargs -0 rm -rdf
 rm minion-$1.zip
 zip -r -9 minion-$1.zip minion-release-$1
