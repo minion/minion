1c005962be0f8909920eae9dafc9145a8ad3ff1f
Christopher Jefferson <chris@bubblescope.net>
2008-02-26 16:07:44 +0000

    Update svn version file to work without a svn checkout
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@1179 477b24dd-e91e-0410-aa49-980f3df0729b

--- mini-scripts/get_svn_version.sh
+++ mini-scripts/get_svn_version.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-ver=`svn info | grep Revision | cut -b11-`
-date=`svn info | grep "Last Changed Date" | cut -b20-`
+ver=`svn info 2>/dev/null | grep Revision | cut -b11-`  
+date=`svn info 2>/dev/null | grep "Last Changed Date" | cut -b20-` 
 if [ "X$ver" == "X" ]; then
-  echo This is not a valid svn checkout
+  echo \#define SVN_VER \"Not generated from a svn checkout.\" > $1
+  echo \#define SVN_DATE \"\" >> $1
 else
 
   #Next two lines read first line of file into 'line'
-  exec 3< $1
-  read <&3 line
+  if [ -e $1 ]
+  then
+    exec 3< $1
+    read <&3 line
+  fi
   newline="#define SVN_VER \"${ver}\""
   if [ "${newline}" != "${line}" ]; then
     # Have to update file
