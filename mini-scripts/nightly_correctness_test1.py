e1898d65e6b58f4ca5fe34bafb80077e42a0b5cb
Peter Nightingale <pwn1@users.noreply.github.com>
2008-03-12 14:26:19 +0000

    Add 2 scripts to nightly test minion and minion-debug respectively.
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@1201 477b24dd-e91e-0410-aa49-980f3df0729b

---
+++ mini-scripts/nightly_correctness_test1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+# Script to run the nightly correctness testing scripts.
+
+from os import *
+from sys import exit
+import sendemail
+
+# of course this is bad. The minion directory should be a command line option.
+homedir="/cs/home/rsch/pn/"
+miniondir="minion-svn1/minion"
+
+chdir(homedir+miniondir)   #cd /home/pn/minion-svn/minion
+system("rm Makefile*")   # get rid of stale makefiles that were causing problems.
+
+retval=0
+retval+=system("svn update")
+retval+=system("make veryclean")
+
+if retval!=0:
+    sendemail.mail("An error occurred when checking out or doing make veryclean.")
+    exit(1)
+
+retval+=system("make minion")
+
+if retval!=0:
+    sendemail.mail("An error occurred when building minion.")
+    exit(1)
+
+chdir("test_instances")
+retval+=system("./run_tests.sh ../bin/minion")
+
+if retval!=0:
+    sendemail.mail("An error occurred when running test_instances/run_tests.sh for minion")
+    exit(1)
+
+chdir(homedir+miniondir)   #cd /home/pn/minion-svn/minion
+
+system("mini-scripts/testallconstraints.py --numtests=500 --email --minion=bin/minion") 
+
