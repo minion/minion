2b8f0095d67dece59c96988e1eed9ad3e7a4862a
Christopher Jefferson <chris@bubblescope.net>
2008-04-25 19:31:05 +0000

    Fix problem caused by removed header files in Makefile.dep
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@1350 477b24dd-e91e-0410-aa49-980f3df0729b

--- mini-scripts/make_depend.sh
+++ mini-scripts/make_depend.sh
@@ -12,11 +12,11 @@ do
     # This nasty line just changes the extension...
     BINFILE=${file%.cpp}.o
   #  BINFILE=${BINFILE#./}
-    g++ -MM -MT \$\(OBJDIR\)/${BINFILE} minion/$file &> /dev/null
+    g++ -MM -MT -MP \$\(OBJDIR\)/${BINFILE} minion/$file &> /dev/null
     if [ "$?" -eq "0" ]; then
       # Have to run this twice, as storing the result in a variable
       # Loses line endings.
-      g++ -MM -MT \$\(OBJDIR\)/${BINFILE} minion/$file >> Makefile.dep.temp
+      g++ -MM -MT -MP \$\(OBJDIR\)/${BINFILE} minion/$file >> Makefile.dep.temp
     fi
   fi 
 done
