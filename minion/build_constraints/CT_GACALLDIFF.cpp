a919fe0445f04ff2e11684fb189bd370f251056c
Peter Nightingale <pwn1@users.noreply.github.com>
2008-05-29 19:15:36 +0000

    Fix gacalldiff
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@1437 477b24dd-e91e-0410-aa49-980f3df0729b

---
+++ minion/build_constraints/CT_GACALLDIFF.cpp
@@ -0,0 +1,12 @@
+#include "../minion.h"
+/* Minion Constraint Solver
+   http://minion.sourceforge.net
+   
+   For Licence Information see file LICENSE.txt 
+
+   $Id$
+*/
+
+#include "../constraints/constraint_gacalldiff.h"
+BUILD_STATIC_CT(CT_GACALLDIFF, 1)
+
