dbb454364798dd176b60c4c4ad8469bb2cd463af
Peter Nightingale <pwn1@users.noreply.github.com>
2008-06-27 14:02:01 +0000

    Add manual to release
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@1604 477b24dd-e91e-0410-aa49-980f3df0729b

--- release-scripts/make-release.sh
+++ release-scripts/make-release.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#./build-all.sh MYFLAGS="-arch ppc -arch i386"
 cd ..
 rm -rf minion-release-$1
 mkdir minion-release-$1
 cp bin/* minion-release-$1
 strip minion-release-$1/*
 cp -r docs/htmlhelp minion-release-$1
+cp manual/Manual.pdf minion-release-$1
 cp LICENSE.txt minion-release-$1
 cp -r test_instances minion-release-$1
 cp -r benchmarks minion-release-$1
