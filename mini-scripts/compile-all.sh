ea31b2872403982c574c532b8a59356aef1ca881
Lars Kotthoff <larsko@cs.st-andrews.ac.uk>
2008-10-15 14:56:41 +0000

    fixed typo in compile-all.sh and added check which will skip compilation of further targets when one failed
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@1802 477b24dd-e91e-0410-aa49-980f3df0729b

--- mini-scripts/compile-all.sh
+++ mini-scripts/compile-all.sh
@@ -6,9 +6,13 @@ for CONFIG in \
 	"-DDEBUG=0 -DUNOPTIMISED=0 -DPROFILE=0 -DSMALL=0 -DINFO=1 -DREENTER=0 -DSTATIC=0 -DQUICK=0" \
 	"-DDEBUG=0 -DUNOPTIMISED=0 -DPROFILE=0 -DSMALL=0 -DINFO=0 -DREENTER=1 -DSTATIC=0 -DQUICK=0" \
 	"-DDEBUG=0 -DUNOPTIMISED=0 -DPROFILE=0 -DSMALL=0 -DINFO=0 -DREENTER=0 -DSTATIC=1 -DQUICK=0" \
-	"-DDEBUG=0 -DUNOPTIMISED=0 -DPROFILE=0 -DSMALL=0 -DINFO=0 -DREENTER=0 -DSTATIC=1 -DQUICK=1"
+	"-DDEBUG=0 -DUNOPTIMISED=0 -DPROFILE=0 -DSMALL=0 -DINFO=0 -DREENTER=0 -DSTATIC=0 -DQUICK=1"
 do
 	cmake $CONFIG .
 	make clean
-	make minion $@ || echo "Compilation failed for configuration $CONFIG."
+	make minion $@
+	if [ $? -ne 0 ]; then
+		echo "Compilation failed for configuration $CONFIG."
+		break
+	fi
 done
