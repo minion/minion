cd0388e60fb5b07aeeb1c45c54728db4824510bb
Christopher Jefferson <chris@bubblescope.net>
2008-10-16 19:46:11 +0000

    Script to turn dreadnaut output into gap input
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@1835 477b24dd-e91e-0410-aa49-980f3df0729b

---
+++ mini-scripts/dread2gap.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import sys
+
+print("G := Group( ()")
+for line in sys.stdin:
+        if(line[0] == "("):
+                print(","+line.replace(" ",","))
+print(");");
+print("Print(Size(G),\" \");");
