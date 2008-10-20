93dc70fdb6161850aa9f3fad0a3ea2a8dc074873
Peter Nightingale <pwn1@users.noreply.github.com>
2008-10-20 12:59:20 +0000

    Make occurrence (&leq/geq) negatable.
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@1855 477b24dd-e91e-0410-aa49-980f3df0729b

--- minion/constraints/constraint_occurrance.h
+++ minion/constraints/constraint_occurrance.h
@@ -90,6 +90,301 @@ help constraints occurrence
 help constraints occurrenceleq
 */
 
+// Negated occurrence; used in reverse_constraint for OccurrenceEqualConstraint
+template<typename VarArray, typename Val, typename ValCount>
+struct NotOccurrenceEqualConstraint : public AbstractConstraint
+{
+  virtual string constraint_name()
+  { return "NotOccurrenceEqual"; }
+  
+  typedef typename VarArray::value_type VarRef;
+  
+  ReversibleInt occurrences_count;
+  ReversibleInt not_occurrences_count;
+  VarArray var_array;
+  
+  ValCount val_count;
+  Val value;
+  
+  NotOccurrenceEqualConstraint(StateObj* _stateObj, const VarArray& _var_array, const Val& _value, const ValCount& _val_count) :
+    AbstractConstraint(_stateObj), occurrences_count(_stateObj), not_occurrences_count(_stateObj),
+    var_array(_var_array), val_count(_val_count), value(_value), trigger1index(-1), trigger2index(-1)
+  { }
+  
+  // Put two assignment triggers on the vector, and one on val_count.
+  // When all vars in X are assigned, remove count(X=v) from val_count
+  
+  // When val_count is assigned and all but one of the vector are assigned,
+  // consider the remaining one in the vector, and either fix it to v or 
+  // remove v from its domain to avoid the value of val_count.
+  
+  int dynamic_trigger_count()
+  { // two moving assignment triggers.
+	return 2;
+  }
+  
+  virtual triggerCollection setup_internal()
+  {
+    triggerCollection t;
+    t.push_back(make_trigger(val_count, Trigger(this, -1), Assigned));
+    return t;
+  }
+  
+  virtual BOOL check_assignment(DomainInt* v, int v_size)
+  {
+    D_ASSERT(v_size == var_array.size() + 1);
+    DomainInt count = 0;  
+    for(int i = 0; i < v_size - 1; ++i)
+      count += (*(v + i) == value);
+    return count != *(v + v_size - 1);
+  }
+  
+  virtual vector<AnyVarRef> get_vars()
+  {
+    vector<AnyVarRef> vars;
+	vars.reserve(var_array.size() + 1);
+	for(unsigned i = 0; i < var_array.size(); ++i)
+	  vars.push_back(AnyVarRef(var_array[i]));
+    vars.push_back(AnyVarRef(val_count));
+	return vars;
+  }
+  
+  
+  PROPAGATE_FUNCTION(int z, DomainDelta)
+  {
+      // val_count has been assigned.
+      D_ASSERT(z==-1);
+      DynamicTrigger* dt = dynamic_trigger_start();
+      if(trigger1index==-1 || var_array[trigger1index].isAssigned())
+      {
+          trigger1index=watch_unassigned_in_vector(-1, trigger1index, dt);
+          if(trigger1index==-1)
+          {
+              valcount_assigned();
+              return;
+          }
+      }
+      if(trigger2index==-1 || var_array[trigger2index].isAssigned())
+      {
+          trigger2index=watch_unassigned_in_vector(trigger1index, trigger2index, dt+1);
+          if(trigger2index==-1)
+          {
+              valcount_assigned();
+              return;
+          }
+      }
+  }
+  
+  PROPAGATE_FUNCTION(DynamicTrigger* trig)
+  {
+      DynamicTrigger* dt = dynamic_trigger_start();
+      if(trig==dt || trigger1index==-1)
+      {
+          if(val_count.isAssigned())
+          {
+              // make sure both triggers are in place.
+              trigger1index=watch_unassigned_in_vector(-1, trigger1index, dt);
+              if(trigger1index==-1)
+              {
+                  valcount_assigned();
+                  return;
+              }
+              if(trigger2index==-1 || var_array[trigger2index].isAssigned())
+              {
+                  trigger2index=watch_unassigned_in_vector(trigger1index, trigger2index, dt+1);
+                  if(trigger2index==-1)
+                  {
+                      valcount_assigned();
+                      return;
+                  }
+              }
+          }
+          else
+          {
+              trigger1index=watch_unassigned_in_vector(-1, trigger1index, dt);
+              if(trigger1index==-1)
+              {
+                  vector_assigned();
+                  return;
+              }
+          }
+          return;
+      }
+      D_ASSERT(trig==dt+1);
+      if(!val_count.isAssigned())
+      {   // don't need two triggers.
+          trig->remove();
+          trigger2index=-1;
+          return;
+      }
+      
+      if(var_array[trigger1index].isAssigned())
+      {
+          // just wait for the other trigger, then both triggers will be repositioned. lazy coding!
+          return;
+      }
+      
+      trigger2index=watch_unassigned_in_vector(trigger1index, trigger2index, dt+1);
+      if(trigger2index==-1)
+      {
+          valcount_assigned();
+      }
+  }
+  
+  // unfinished new stuff starts here.
+  
+  int watch_unassigned_in_vector(int avoidindex, int oldsupport, DynamicTrigger* dt)
+  {
+      // move dt to an index other than avoidindex, or return -1.
+      int newsupport=oldsupport+1;
+      for( ; newsupport<var_array.size(); newsupport++)
+      {
+          if(newsupport!=avoidindex)
+          {
+              if(!var_array[newsupport].isAssigned())
+              {
+                  var_array[newsupport].addDynamicTrigger(dt, Assigned);
+                  return newsupport;
+              }
+          }
+      }
+      
+      for(newsupport=0; newsupport<=oldsupport; newsupport++)
+      {
+          if(newsupport!=avoidindex)
+          {
+              if(!var_array[newsupport].isAssigned())
+              {
+                  var_array[newsupport].addDynamicTrigger(dt, Assigned);
+                  return newsupport;
+              }
+          }
+      }
+      return -1;
+  }
+  
+  int trigger1index;
+  int trigger2index;
+  
+  void vector_assigned()
+  {
+      // count occurrences of val
+      int occ=0;
+      for(int i=0; i<var_array.size(); i++)
+      {
+          if(var_array[i].getAssignedValue()==value)
+              occ++;
+      }
+      if(val_count.inDomain(occ))
+      {
+          if(!val_count.isBound())
+          {
+              val_count.removeFromDomain(occ);
+          }
+          else
+          {
+              // go into watching-the-bounds mode.
+              FAIL_EXIT("no support for watching the bounds in not-occurrence");
+          }
+      }
+  }
+  
+  void valcount_assigned()
+  {
+      // valcount, and all but one (or all) of the vector, are assigned.
+      // count occurrences of val
+      int occ=0;
+      int unassigned=-1;
+      D_ASSERT(val_count.isAssigned());
+      for(int i=0; i<var_array.size(); i++)
+      {
+          if(var_array[i].isAssigned())
+          {
+              if(var_array[i].getAssignedValue()==value)
+              {
+                  occ++;
+              }
+          }
+          else
+          {
+              D_ASSERT(unassigned==-1);
+              unassigned=i;
+          }
+      }
+      
+      // and the rest.
+      if(unassigned==-1)
+      {
+          // just check, everything is assigned.
+          if(occ==val_count.getAssignedValue())
+          {
+              getState(stateObj).setFailed(true);
+          }
+      }
+      else
+      {
+          if(occ==val_count.getAssignedValue())
+          { // need another occurrence of the value
+              var_array[unassigned].propagateAssign(value);
+          }
+          else if(occ+1 == val_count.getAssignedValue())
+          { // not allowed to have another value.
+              var_array[unassigned].removeFromDomain(value);
+          }
+      }
+  }
+  
+  virtual void full_propagate()
+  {
+    DynamicTrigger* dt = dynamic_trigger_start();
+    trigger1index=watch_unassigned_in_vector(-1, 0, dt);
+    if(trigger1index==-1)
+        vector_assigned();
+    
+    if(val_count.isAssigned())
+    {
+        // watch a second place in the vector.
+        trigger2index=watch_unassigned_in_vector(trigger1index, trigger1index, dt+1);
+        if(trigger2index==-1)
+            valcount_assigned();
+    }
+  }
+  
+   // Getting a satisfying assignment here is too hard, we don't want to have to
+   // build a matching.
+  virtual void get_satisfying_assignment(box<pair<int,DomainInt> >& assignment)
+  {
+    MAKE_STACK_BOX(c, DomainInt, var_array.size() + 1);
+
+    for(int i = 0; i < var_array.size(); ++i)
+    {
+      if(!var_array[i].isAssigned()) 
+      {  
+        assignment.push_back(make_pair(i, var_array[i].getMin()));
+        assignment.push_back(make_pair(i, var_array[i].getMax()));
+        return;
+      }
+      else
+        c.push_back(var_array[i].getAssignedValue());
+    }
+    
+    if(!val_count.isAssigned()) 
+    {  
+      assignment.push_back(make_pair(var_array.size(), val_count.getMin()));
+      assignment.push_back(make_pair(var_array.size(), val_count.getMax()));
+      return;
+    }
+    else
+      c.push_back(val_count.getAssignedValue());
+    
+    if(check_assignment(c.begin(), c.size()))
+    {  // Put the complete assignment in the box.
+      for(int i = 0; i < var_array.size() + 1; ++i)
+        assignment.push_back(make_pair(i, c[i])); 
+    }
+  }
+};
+
 template<typename VarArray, typename Val>
 struct ConstantOccurrenceEqualConstraint : public AbstractConstraint
 {
@@ -269,7 +564,23 @@ struct ConstantOccurrenceEqualConstraint : public AbstractConstraint
         assignment.push_back(make_pair(i, c[i])); 
     }
   }
- 	
+
+  AbstractConstraint* reverse_constraint()
+  {
+      // This constraint actually constrains the occurrences of value to an an interval
+      // [val_count_min, val_count_max]. But it's apparently only used for less-than
+      // and greater-than. So identify the less-than case and make a greater-than, etc.
+      if(val_count_min==0)
+      {
+          return new ConstantOccurrenceEqualConstraint<VarArray, Val>(stateObj, var_array, value, val_count_max+1, var_array.size());
+      }
+      if(val_count_max==var_array.size())
+      {
+          return new ConstantOccurrenceEqualConstraint<VarArray, Val>(stateObj, var_array, value, 0, val_count_min-1);
+      }
+      FAIL_EXIT("Unable to negate an occurrence-interval constraint, sorry."); 
+      return NULL;
+  }
 };
 
 template<typename VarArray, typename Val, typename ValCount>
@@ -460,7 +771,11 @@ struct OccurrenceEqualConstraint : public AbstractConstraint
         assignment.push_back(make_pair(i, c[i])); 
     }
   }
- 	
+  
+  AbstractConstraint* reverse_constraint()
+  {
+      return new NotOccurrenceEqualConstraint<VarArray, Val, ValCount>(stateObj, var_array, value, val_count);
+  }
 };
 
 
