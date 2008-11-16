0b20caac2f47efb1967c867639b124808aba9889
Lars Kotthoff <larsko@cs.st-andrews.ac.uk>
2008-11-16 16:28:10 +0000

    scripts to run benchmarks in a hudson-compatible way
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@1969 477b24dd-e91e-0410-aa49-980f3df0729b

---
+++ benchmarks/scripts/runall-hudson.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+python benchmarks/scripts/benchmark-hudson.py $1 bibd benchmarks/Bibd/bibdline?.minion
+python benchmarks/scripts/benchmark-hudson.py $1 bibd-watched benchmarks/Bibd/bibdline?watched.minion
+python benchmarks/scripts/benchmark-hudson.py $1 solitaire benchmarks/solitaire/*
+python benchmarks/scripts/benchmark-hudson.py $1 SAT benchmarks/SAT/*
+python benchmarks/scripts/benchmark-hudson.py $1 quasigroup-element benchmarks/Quasigroup/qg-element*
+python benchmarks/scripts/benchmark-hudson.py $1 quasigroup-gacelement benchmarks/Quasigroup/qg-gacelement*
+python benchmarks/scripts/benchmark-hudson.py $1 quasigroup-watchelement benchmarks/Quasigroup/qg-watchelement*
+python benchmarks/scripts/benchmark-hudson.py $1 graceful-simple benchmarks/graceful/*simple.minion
+python benchmarks/scripts/benchmark-hudson.py $1 graceful-table benchmarks/graceful/*table.minion
