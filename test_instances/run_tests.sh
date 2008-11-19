2e010eebfbfb7bf956954b89af1aedbc4fe7b653
Christopher Jefferson <chris@bubblescope.net>
2008-11-19 18:46:25 +0000

    Add test for reified and add support for counting nodes
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@1985 477b24dd-e91e-0410-aa49-980f3df0729b

--- test_instances/run_tests.sh
+++ test_instances/run_tests.sh
@@ -7,7 +7,7 @@ if [ $# -lt 1 ]; then
 fi
 
 if [ ! -x $1 ]
-then
+  then
   echo $1 either doesn\'t exist, or isn\'t executable.
   exit 0
 fi
@@ -29,100 +29,112 @@ for i in *.minion; do
 
   # This gives the value that the program should return.
   if grep -q "#FAIL" $i;
-  then 
+    then 
     correctoutput=0
-  else
+    else
     correctoutput=1  
   fi
-  
+
   if grep -q "#BUG" $i;
-  then 
+    then 
     bug=1
-  else 
+    else 
     bug=0
   fi
-  
+
   if grep -q "#TEST SOLCOUNT" $i;
-  then
+    then
     numsols=`$exec $i -findallsols $* 2>/dev/null | ../mini-scripts/solutions.sh`
     testnumsols=`grep "#TEST SOLCOUNT" $i  | awk '{print $3}' | tr -d '\015' `
-	if [[ "$numsols" != "$testnumsols" ]]; then
-	  testpass=0
-	  errormess="Got '${numsols}' instead of '${testnumsols}' solutions in $i"
-	else
-	  testpass=1
-	fi
+    if [[ "$numsols" != "$testnumsols" ]]; then
+      testpass=0
+      errormess="Got '${numsols}' instead of '${testnumsols}' solutions in $i"
+    else
+      testpass=1
+    fi
   else
     if grep -q "#TEST CHECKONESOL" $i; then
       sol=`$exec $i $* 2>/dev/null | ../mini-scripts/print_sol.sh`
       # That "tr" is just to deal with line ending problems.
       testsol=`grep "#TEST CHECKONESOL" $i | awk '{$1 = ""; $2 = ""; print }' | tr -d '\015' `
-      
+
       # This horrible mess just strips the given solutions into a comparable
       # format. It turns " 1   2   3   4 " into "1,2,3,4". 
       sol=`echo $sol | sed -e "s/^ \{1,\}//;s/ \{1,\}$//;s/ \{1,\}/,/g;"`
       testsol=`echo $testsol | sed -e "s/^ \{1,\}//;s/ \{1,\}$//;s/ \{1,\}/,/g;"`
       if [[ "$sol" = "$testsol" ]]; then
         testpass=1
-      else
+        else
         testpass=0
         errormess="Got '${sol}' instead of '${testsol}' as solution in $i"
       fi
     else
-      echo Test $i is not well-formed.
-      exit 1
-    fi  
-  fi
-  
-  if [ "$bug" = "0" ]; then
-    if [ "$testpass" = "$correctoutput" ]; then
-      pass=$(($pass + 1))
-    else
-      if [ "$correctoutput" = "0" ]; then
-        echo Expected $i to fail.
+      if grep -q "#TEST NODECOUNT" $i;
+        then
+        numnodes=`$exec $i $* 2>/dev/null | ../mini-scripts/nodecount.sh`
+        testnumnodes=`grep "#TEST NODECOUNT" $i  | awk '{print $3}' | tr -d '\015' `
+        if [[ "$numnodes" != "$testnumnodes" ]]; then
+          testpass=0
+          errormess="Got '${numnodes}' instead of '${testnumnodes}' search nodes in $i"
+        else
+          testpass=1
+        fi
       else
-        echo $errormess
+        echo Test $i is not well-formed.
+        exit 1
+      fi  
+    fi
+  fi
+
+    if [ "$bug" = "0" ]; then
+      if [ "$testpass" = "$correctoutput" ]; then
+        pass=$(($pass + 1))
+        else
+        if [ "$correctoutput" = "0" ]; then
+          echo Expected $i to fail.
+          else
+          echo $errormess
+        fi
       fi
-    fi
-  else
-    if [ "$testpass" = "$correctoutput" ]; then
-      echo $i passed, but is supposed to be buggy!
-      unexpectedpass=$(($unexpectedpass + 1))
-    else
-      expectedfail=$(($expectedfail + 1))
-    fi
-  fi  
-done
+      else
+      if [ "$testpass" = "$correctoutput" ]; then
+        echo $i passed, but is supposed to be buggy!
+        unexpectedpass=$(($unexpectedpass + 1))
+        else
+        expectedfail=$(($expectedfail + 1))
+      fi
+    fi  
+  done
 
-echo
-echo $pass of $j tests successful.
-echo $(($j - $pass - $expectedfail)) tests failed due to unexpected errors.
-echo $expectedfail tests failed due to expected errors.
-echo $unexpectedpass tests passed unexpectedly.
+  echo
+  echo $pass of $j tests successful.
+  echo $(($j - $pass - $expectedfail)) tests failed due to unexpected errors.
+  echo $expectedfail tests failed due to expected errors.
+  echo $unexpectedpass tests passed unexpectedly.
 
-failed=0
+  failed=0
 
-# The following tests take too long!
-./do_random_tests.sh 3 $exec $* -randomiseorder
-failed=$(($failed + $?))
-./do_random_tests.sh 3 $exec $* -varorder random
-failed=$(($failed + $?))
-./do_random_tests.sh 1 $exec $* -varorder conflict
-failed=$(($failed + $?))
-./do_random_tests.sh 1 $exec $* -varorder static
-failed=$(($failed + $?))
-./do_random_tests.sh 1 $exec $* -varorder sdf
-failed=$(($failed + $?))
-./do_random_tests.sh 3 $exec $* -varorder sdf-random
-failed=$(($failed + $?))
-./do_random_tests.sh 1 $exec $* -varorder srf
-failed=$(($failed + $?))
-./do_random_tests.sh 3 $exec $* -varorder srf-random
-failed=$(($failed + $?))
-./do_random_tests.sh 1 $exec $* -varorder ldf
-failed=$(($failed + $?))
-./do_random_tests.sh 3 $exec $* -varorder ldf-random
-failed=$(($failed + $?))
+  # The following tests take too long!
+  ./do_random_tests.sh 3 $exec $* -randomiseorder
+  failed=$(($failed + $?))
+  ./do_random_tests.sh 3 $exec $* -varorder random
+  failed=$(($failed + $?))
+  ./do_random_tests.sh 1 $exec $* -varorder conflict
+  failed=$(($failed + $?))
+  ./do_random_tests.sh 1 $exec $* -varorder static
+  failed=$(($failed + $?))
+  ./do_random_tests.sh 1 $exec $* -varorder sdf
+  failed=$(($failed + $?))
+  ./do_random_tests.sh 3 $exec $* -varorder sdf-random
+  failed=$(($failed + $?))
+  ./do_random_tests.sh 1 $exec $* -varorder srf
+  failed=$(($failed + $?))
+  ./do_random_tests.sh 3 $exec $* -varorder srf-random
+  failed=$(($failed + $?))
+  ./do_random_tests.sh 1 $exec $* -varorder ldf
+  failed=$(($failed + $?))
+  ./do_random_tests.sh 3 $exec $* -varorder ldf-random
+  failed=$(($failed + $?))
 
-# return 0 iff all tests succeeded
-exit $(($j - $pass - $expectedfail + $?))
+  # return 0 iff all tests succeeded
+  exit $(($j - $pass - $expectedfail + $?))
