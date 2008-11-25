0d78dfa3142bd45fa94d605b08c3d53cf01e887a
Christopher Jefferson <chris@bubblescope.net>
2008-11-25 17:27:08 +0000

    Remove dunused code
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@2018 477b24dd-e91e-0410-aa49-980f3df0729b

--- minion/dynamic_constraints/dynamic_sumor.h
+++ minion/dynamic_constraints/dynamic_sumor.h
@@ -308,21 +308,6 @@ BuildCT_WATCHED_HAMMING(StateObj* stateObj, const T1& t1, const T2& t2, BOOL rei
   { return VecOrCountConDynamic(stateObj, t1, t2, b.constants[0][0]); }
 }
 
-/*
-template<typename VarArray1,  typename VarArray2>
-AbstractConstraint*
-  VecOrLessConDynamic(StateObj* stateObj,const VarArray1& varray1, const VarArray2& varray2)
-  { return new VecNeqDynamic<VarArray1,VarArray2, LessIterated>(stateObj, varray1, varray2); }
-
-BUILD_DYNAMIC_CONSTRAINT2(CT_WATCHED_VEC_OR_LESS, VecOrLessConDynamic)
-
-template<typename VarArray1,  typename VarArray2>
-AbstractConstraint*
-  VecOrAndConDynamic(StateObj* stateObj,const VarArray1& varray1, const VarArray2& varray2)
-  { return new VecNeqDynamic<VarArray1,VarArray2, BothNonZeroIterated>(stateObj, varray1, varray2); }
-
-BUILD_DYNAMIC_CONSTRAINT2(CT_WATCHED_VEC_OR_AND, VecOrAndConDynamic)
-*/
 
 #endif
 
