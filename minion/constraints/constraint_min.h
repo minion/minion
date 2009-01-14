4fd028f0c55f3f7af0ad912caa30c2f26f8d485d
Christopher Jefferson <chris@bubblescope.net>
2009-01-14 14:25:05 +0000

    Fix empty (and length 1) array bugs in max and min
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@2181 477b24dd-e91e-0410-aa49-980f3df0729b

--- minion/constraints/constraint_min.h
+++ minion/constraints/constraint_min.h
@@ -75,7 +75,7 @@ struct MinConstraint : public AbstractConstraint
 	  t.push_back(make_trigger(var_array[i], Trigger(this, -(i + 1)), UpperBound));
 	}
 	t.push_back(make_trigger(min_var, Trigger(this, var_array.size() + 1 ),LowerBound));
-	t.push_back(make_trigger(min_var, Trigger(this, -(int)var_array.size() + 1 ),UpperBound));
+	t.push_back(make_trigger(min_var, Trigger(this, -((int)var_array.size() + 1) ),UpperBound));
 	
 	return t;
   }
@@ -83,93 +83,98 @@ struct MinConstraint : public AbstractConstraint
   //  virtual AbstractConstraint* reverse_constraint()
   
   PROPAGATE_FUNCTION(int prop_val, DomainDelta)
-   {
-	PROP_INFO_ADDONE(Min);
-	if(prop_val > 0)
-	{// Lower Bound Changed
-	  
-	  //Had to add 1 to fix "0th array" problem.
-	  --prop_val;
-	  
-	  if(prop_val == (int)(var_array.size()))  
-	  {
-		DomainInt new_min = min_var.getMin();
-		typename VarArray::iterator end = var_array.end();
-		for(typename VarArray::iterator it = var_array.begin(); it < end; ++it)
-		  (*it).setMin(new_min);
-	  }
-	  else
-	  {
-	    typename VarArray::iterator it = var_array.begin();
-		typename VarArray::iterator end = var_array.end();
-		DomainInt min = it->getMin();
-		++it;
-		for(; it < end; ++it)
-		{
-		  DomainInt it_min = it->getMin();
-		  if(it_min < min)
-			min = it_min;
-		}
-		min_var.setMin(min);
-	  }
-	}
-	else
-	{// Upper Bound Changed
-	  // See above for reason behind "-1".
-	  prop_val = -prop_val - 1;
-	  if(prop_val == (int)(var_array.size()))
-	  {
-		typename VarArray::iterator it = var_array.begin();
-		DomainInt minvar_max = min_var.getMax();
-		while(it != var_array.end() && (*it).getMin() > minvar_max)
-		  ++it;
-		if(it == var_array.end())
-		{
-		  getState(stateObj).setFailed(true);
-		  return;
-		}
-		
-		// Possibly this variable is the only one that can be the minimum
-		typename VarArray::iterator it_copy(it);
-		++it;
-		while(it != var_array.end() && (*it).getMin() > minvar_max)
-		  ++it;
-		if(it != var_array.end())
-		{ // No, another variable can be the minimum
-		  return;
-		}
-		
-		it_copy->setMax(minvar_max);
-	  }
-	  else
-	  {
-		min_var.setMax(var_array[prop_val].getMax());
-	  }
-	}
-	
+  {
+    PROP_INFO_ADDONE(Min);
+    if(prop_val > 0)
+    {// Lower Bound Changed
+
+    //Had to add 1 to fix "0th array" problem.
+      --prop_val;
+
+      if(prop_val == (int)(var_array.size()))  
+      {
+        DomainInt new_min = min_var.getMin();
+        typename VarArray::iterator end = var_array.end();
+        for(typename VarArray::iterator it = var_array.begin(); it < end; ++it)
+          (*it).setMin(new_min);
+      }
+      else
+      {
+        typename VarArray::iterator it = var_array.begin();
+        typename VarArray::iterator end = var_array.end();
+        DomainInt min = it->getMin();
+        ++it;
+        for(; it < end; ++it)
+        {
+          DomainInt it_min = it->getMin();
+          if(it_min < min)
+            min = it_min;
+        }
+        min_var.setMin(min);
+      }
+    }
+    else
+    {// Upper Bound Changed
+      // See above for reason behind "-1".
+      prop_val = -prop_val - 1;
+      if(prop_val == (int)(var_array.size()))
+      {
+        typename VarArray::iterator it = var_array.begin();
+        DomainInt minvar_max = min_var.getMax();
+        while(it != var_array.end() && (*it).getMin() > minvar_max)
+          ++it;
+        if(it == var_array.end())
+        {
+          getState(stateObj).setFailed(true);
+          return;
+        }
+        // Possibly this variable is the only one that can be the minimum
+        typename VarArray::iterator it_copy(it);
+        ++it;
+        while(it != var_array.end() && (*it).getMin() > minvar_max)
+          ++it;
+        if(it != var_array.end())
+        { // No, another variable can be the minimum
+          return;
+        }
+        it_copy->setMax(minvar_max);
+      }
+      else
+      {
+        min_var.setMax(var_array[prop_val].getMax());
+      }
+    }
+
   }
   
-  //  virtual BOOL check_unsat(int i, DomainDelta)
-  //  {
-  
-  
+    
   virtual void full_propagate()
   {
-	int array_size = var_array.size();
-	for(int i = 1;i <= array_size + 1; ++i)
-	{
-	  propagate(i,0);
-	  propagate(-i,0);
-	}
+    int array_size = var_array.size();
+    if(array_size == 0)
+    {
+      getState(stateObj).setFailed(true);
+    }
+    else
+    {
+      for(int i = 1;i <= array_size + 1; ++i)
+      {
+        propagate(i,0);
+        propagate(-i,0);
+      }
+    }
   }
   
   virtual BOOL check_assignment(DomainInt* v, int v_size)
   {
-	D_ASSERT(v_size == var_array.size() + 1);
-	DomainInt min_val = big_constant;
-	for(int i=0;i < v_size - 1;i++)
-	  min_val = min(min_val, v[i]);
-	return min_val == *(v + v_size - 1);
+    D_ASSERT(v_size == var_array.size() + 1);
+    if(v_size == 1)
+      return false;
+      
+    DomainInt min_val = big_constant;
+    for(int i = 0;i < v_size - 1;i++)
+      min_val = min(min_val, v[i]);
+    return min_val == *(v + v_size - 1);
   }
 
   // Bah: This could be much better!
@@ -211,24 +216,24 @@ struct MinConstraint : public AbstractConstraint
   }
   
   // Function to make it reifiable in the lousiest way.
-    virtual AbstractConstraint* reverse_constraint()
-    {
-        vector<AnyVarRef> t;
-        for(int i=0; i<var_array.size(); i++)
-            t.push_back(var_array[i]);
-        t.push_back(min_var);
-        
-        return new CheckAssignConstraint<vector<AnyVarRef>, MinConstraint>(stateObj, t, *this);
-    }
-  
+  virtual AbstractConstraint* reverse_constraint()
+  {
+      vector<AnyVarRef> t;
+      for(int i=0; i<var_array.size(); i++)
+          t.push_back(var_array[i]);
+      t.push_back(min_var);
+      
+      return new CheckAssignConstraint<vector<AnyVarRef>, MinConstraint>(stateObj, t, *this);
+  }
+
   virtual vector<AnyVarRef> get_vars()
   {
     vector<AnyVarRef> vars;
-	vars.reserve(var_array.size() + 1);
-	for(unsigned i = 0; i < var_array.size(); ++i)
-	  vars.push_back(AnyVarRef(var_array[i]));
-	vars.push_back(AnyVarRef(min_var));
-	return vars;
+    vars.reserve(var_array.size() + 1);
+    for(unsigned i = 0; i < var_array.size(); ++i)
+      vars.push_back(AnyVarRef(var_array[i]));
+    vars.push_back(AnyVarRef(min_var));
+    return vars;
   }
 };
 
