7a43747c680878f97bc5f48ed9b622c6d3d7f9e2
Lars Kotthoff <larsko@cs.st-andrews.ac.uk>
2009-11-24 09:23:05 +0000

    tweaking script to get times and nodes to optionally get system time as well

--- mini-scripts/get-times-nodes.sh
+++ mini-scripts/get-times-nodes.sh
@@ -3,7 +3,12 @@
 # arguments are the minion output files to get the times from
 
 for i in $*; do
-    time=`grep "Total Time: " $i | grep -o "[0-9.]\+"`
+    if [ -n $SYSTIME_TOO ]; then
+        time=`grep "Total Time: " $i | grep -o "[0-9.]\+"`
+        time=$time" "`grep "Total System Time: " $i | grep -o "[0-9.]\+"`
+    else
+        time=`grep "Total Time: " $i | grep -o "[0-9.]\+"`
+    fi
     nodes=`grep "Total Nodes: " $i | grep -o "[0-9]\+"`
     echo "$i $time"
     echo "$i $nodes" 1>&2
