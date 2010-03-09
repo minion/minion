4dbdaaa38ddbcd96a4c56bc47a95ab4b6355dd70
Christopher Jefferson <chris@bubblescope.net>
2010-03-09 12:09:56 +0000

    Fix minor typo

---
+++ minion/constraints/constraint_gcc.h
@@ -0,0 +1,62 @@
+/*
+* Minion http://minion.sourceforge.net
+* Copyright (C) 2006-09
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+/** @help constraints;gcc Description 
+The Generalized Cardinality Constraint (GCC) constrains the number of each value
+that a set of variables can take.
+
+gcc([primary variables], [values of interest], [capacity variables])
+
+For each value of interest, there must be a capacity variable, which specifies
+the number of occurrences of the value in the primary variables.
+
+This constraint is new, and its syntax and implementation are not finalised.
+*/
+
+/** @help constraints;gcc Example 
+Suppose the input file had the following vectors of variables defined:
+
+DISCRETE myVec[9] {1..9}
+BOUND cap[9] {0..2}
+
+The following constraint would restrict the occurrence of values 1..9 in myVec
+to be at most 2 each initially, and finally equal to the values of the cap
+vector.
+
+gcc(myVec, [1,2,3,4,5,6,7,8,9], cap)
+*/
+
+/** @help constraints;gcc Notes 
+This constraint enforces a hybrid consistency. It reads the bounds of the
+capacity variables, then enforces GAC over the primary variables only.  Then the
+bounds of the capacity variables are updated using flow algorithms similar to
+those proposed by Quimper et al, Improved Algorithms for the Global Cardinality
+Constraint.
+
+This constraint provides stronger propagation to the capacity variables than the
+gccweak constraint.
+*/
+
+#ifndef CONSTRAINT_GCC_H_PQWOEI
+#define CONSTRAINT_GCC_H_PQWOEI
+
+#include "gcc_common.h"
+
+#endif
---
+++ tester.cxx
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <istream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    if(argc != 2) abort();
+    int choice = atoi(argv[1]);
+
+    const int num = 1000;
+    cout << "MINION 3" << endl;
+    cout << "**TUPLELIST**" << endl;
+    cout << "mytab 4 3 0 0 0 0 1 0 1 0 0 1 1 1" << endl;
+    cout << "**VARIABLES**" << endl;
+    cout << "BOOL v[" << num << "]" << endl;
+    cout << "**CONSTRAINTS**" << endl;
+    cout << "sumleq(v, 50)" << endl;
+    cout << "sumgeq(v, 51)" << endl;
+
+    for(int i = 0; i < 2000; ++i)
+    {
+        int x1,x2,x3;
+        x1 = rand() % num;
+        x2 = rand() % num;
+        x3 = rand() % num;
+
+
+        switch(choice)
+        {
+            case 0:
+        printf("test([v[%d],v[%d],v[%d]])\n", x1, x2, x3);
+        break;
+            case 1:
+        printf("table([v[%d],v[%d],v[%d]], mytab)\n", x1 ,x2, x3);
+        break;
+            case 2:
+        printf("min([v[%d],v[%d]], v[%d])\n", x1, x2, x3);
+        break;
+        default: abort();
+        }
+    }
+    cout << "**EOF**" << endl;
+
+
+}
