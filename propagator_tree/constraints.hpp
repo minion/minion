0f02b92f40de1913437655bea3930df8d3f08485
Christopher Jefferson <chris@bubblescope.net>
2010-03-17 15:09:22 +0000

    Add chris' constraint generator

---
+++ propagator_tree/constraints.hpp
@@ -0,0 +1,68 @@
+#include <array>
+using namespace std;
+
+
+struct AndCon
+{
+    bool operator()(array<int, 3> a)
+    {
+        return (a[0] && a[1]) == a[2];
+    }
+
+    static const int vcount = 3;
+    static const int domsize = 2;
+};
+
+struct NotAndAndCon
+{
+    bool operator()(array<int, 4> a)
+    {
+        return (a[0] && a[1]) != (a[2] && a[3]);
+    }
+};
+
+struct ReifyDiffEqualConCon
+{
+    bool operator()(array<int, 5> a)
+    {
+        int sum = a[1]+a[2]+a[3]+a[4];
+
+        return a[0] || (sum <= 2);
+    }
+
+    static const int vcount = 5;
+    static const int domsize = 2;
+};
+
+struct EqualReifyNotEqual
+{
+    bool operator()(array<int, 4> a)
+    {
+        return !(a[0] == a[1]) || (a[2] != a[3]);
+    }
+
+    static const int vcount = 4;
+    static const int domsize = 2;
+};
+
+struct GACSum
+{
+    bool operator()(array<int, 3> a)
+    {
+
+        return a[0] + a[1] + a[2] < 4;     
+    }
+
+    static const int vcount = 3;
+    static const int domsize = 3;
+};
+
+//typedef ReifyDiffEqualConCon CurrentConstraint;
+//typedef EqualReifyNotEqual CurrentConstraint;
+typedef GACSum CurrentConstraint;
+
+static const int vcount = CurrentConstraint::vcount;
+static const int domsize = CurrentConstraint::domsize;
+static const int dcount = domsize * vcount;
+
+
