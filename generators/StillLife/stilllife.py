a71191a5d5174174302007f52e6bb1ef8b618592
Peter Nightingale <pwn1@users.noreply.github.com>
2010-04-01 11:34:44 +0100

    improve life generator

--- generators/StillLife/stilllife.py
+++ generators/StillLife/stilllife.py
@@ -6,14 +6,14 @@ print "MINION 3"
 
 print "**VARIABLES**"
 
-n=17
+n=10
 usetest=True
 usetable=False
 usesum=False
 
 
 layers=3
-life=True
+life=False
 
 # settings to find pulsar 
 
@@ -21,13 +21,12 @@ if not life:
     # If we're not modelling life, then it's still life, and we have 1 layer.
     layers=1
 
-print "BOOL l[%d,%d,%d]" %(n+2,n+2, layers)
+print "BOOL l[%d,%d,%d]" %(n+4,n+4, layers)
 
 if usesum:
-    print "DISCRETE sums[%d,%d,%d] {0..8}"%(n+2, n+2, layers)
+    print "DISCRETE sums[%d,%d,%d] {0..8}"%(n+4, n+4, layers)
 
-if not life:
-    print "BOUND maxvar {0..%d}" %(n**2)
+print "BOUND maxvar {0..%d}" %(n**2)
 
 print "**TUPLELIST**"
 
@@ -82,7 +81,7 @@ print " ".join(map(lambda a:str(a), [table[i][j] for i in range(len(table)) for 
 
 print "sumlink 10 2   0 0 1 0 2 0 2 1 3 1 4 0 5 0 6 0 7 0 8 0"
 
-print "sumlinklife 10 2"
+print "sumlinklife 18 3"
 for a in range(9):
     for b in range(2):
         for c in range(2):
@@ -98,19 +97,23 @@ print "**CONSTRAINTS**"
 
 for i in range(layers):
     print "sumleq(l[0,_,%d], 0)"%i
-    print "sumleq(l[%d,_,%d], 0)"%(n+1,i)
+    print "sumleq(l[1,_,%d], 0)"%i
+    print "sumleq(l[%d,_,%d], 0)"%(n+2,i)
+    print "sumleq(l[%d,_,%d], 0)"%(n+3,i)
     print "sumleq(l[_,0,%d], 0)"%i
-    print "sumleq(l[_,%d,%d], 0)"%(n+1,i)
+    print "sumleq(l[_,1,%d], 0)"%i
+    print "sumleq(l[_,%d,%d], 0)"%(n+2,i)
+    print "sumleq(l[_,%d,%d], 0)"%(n+3,i)
     
     if usesum:
         print "sumleq(sums[0,_,%d], 0)"%i
-        print "sumleq(sums[%d,_,%d], 0)"%(n+1,i)
+        print "sumleq(sums[%d,_,%d], 0)"%(n+3,i)
         print "sumleq(sums[_,0,%d], 0)"%i
-        print "sumleq(sums[_,%d,%d], 0)"%(n+1,i)
+        print "sumleq(sums[_,%d,%d], 0)"%(n+3,i)
 
 for layer in range(layers):
-    for i in range(1, n+1):
-        for j in range(1, n+1):
+    for i in range(1, n+3):
+        for j in range(1, n+3):
             st=""
             for k in range(i-1, i+2):
                 for l in range(j-1, j+2):
@@ -119,10 +122,7 @@ for layer in range(layers):
             
             st+="l[%d, %d, %d],"%(i,j, layer)
             if life:
-                if layer== (layers-1):
-                    st+="l[%d, %d, %d],"%(i,j, 0)
-                else:
-                    st+="l[%d, %d, %d],"%(i,j, layer+1)
+                st+="l[%d, %d, %d],"%(i,j, (layer+1)%layers)
             
             if usetest:
                 print "test([%s])"%(st) 
@@ -139,7 +139,7 @@ for layer in range(layers):
                     print "lighttable([sums[%d, %d, %d], l[%d, %d, %d]], sumlink)"%(i,j,layer, i,j, layer)
                 else:
                     print "lighttable([sums[%d, %d, %d], l[%d, %d, %d], l[%d, %d, %d]], sumlinklife)"%(i,j,layer, i,j, layer,i,j,(layer+1)%layers)
-            
+
 if not life:
     print "sumleq(l[_,_,0], maxvar)"
     print "sumgeq(l[_,_,0], maxvar)"
@@ -149,8 +149,27 @@ if not life:
 else:
     # to be interesting the first layer should have something on it. 
     # two dots will just die, so at least 3 are required.
-    print "sumgeq(l[_,_,0], 3)"
+    #print "sumgeq(l[_,_,0], 3)"
+    print "sumleq(l[_,_,_], maxvar)"
+    print "sumgeq(l[_,_,_], maxvar)"
     
+    print "**SEARCH**"
+    print "MAXIMIZING maxvar"
+    
+
+print "PRINT [["
+for layer in range(layers):
+    for i in range(0, n+4):
+        for j in range(0, n+4):
+            print "l[%d, %d, %d],"%(i,j,layer)
+        print "],["
+    print "],["
+
+print "]]"
+
+# val order
+print "VARORDER [l[_,_,0]]"   # should only need to branch on layer 0.
+print "VALORDER ["+(" ".join(["d" for i in range((n+4)**2)]))+" a]"
 
 print "**EOF**"
 
