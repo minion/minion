f1d281b2ba746d67a5c197ddf68a6f569b429a01
Christopher Jefferson <chris@bubblescope.net>
2011-10-24 11:11:01 +0100

    Fix bibd testing, now SAC branches on all variables

--- test_instances/special_tests/special_tests.sh
+++ test_instances/special_tests/special_tests.sh
@@ -29,17 +29,17 @@ if [[ "`$exec test_gzip.minion.gz | $solscript`" != "1" ]]; then
   exit 1
 fi
 
-if [[ "`$exec bibd.minion.bz2 -preprocess SAC | grep ^SAC | awk '{print $3}'`" != "13" ]]; then
+if [[ "`$exec bibd.minion.bz2 -preprocess SAC | grep ^SAC | awk '{print $3}'`" != "36" ]]; then
   echo SAC test failed
   exit 1
 fi
 
-if [[ "`$exec bibd.minion.bz2 -preprocess SSAC | grep ^SAC | awk '{print $3}'`" != "13" ]]; then
+if [[ "`$exec bibd.minion.bz2 -preprocess SSAC | grep ^SAC | awk '{print $3}'`" != "36" ]]; then
   echo SSAC test 1 failed
   exit 1
 fi
 
-if [[ "`$exec bibd.minion.bz2 -preprocess SSAC | grep ^SSAC | awk '{print $3}'`" != "54" ]]; then
+if [[ "`$exec bibd.minion.bz2 -preprocess SSAC | grep ^SSAC | awk '{print $3}'`" != "244" ]]; then
   echo SSAC test 2 failed
   exit 1
 fi
