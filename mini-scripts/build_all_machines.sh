6c6a4b1bd1e823d0004c8c9c633116940b6f809c
Christopher Jefferson <chris@bubblescope.net>
2012-01-20 14:02:28 +0000

    Add new build_all_machines script

--- mini-scripts/build_all_machines.sh
+++ mini-scripts/build_all_machines.sh
@@ -2,7 +2,8 @@
 mkdir machines
 rm -rf machines/*
 initial_path=`pwd`
-for i in lifeImmigration lifeBriansBrain life life3d labs_three labs pegsol and_constraint labs;
+# life3d 
+for i in life labs_three labs pegsol and_constraint labs;
 do
     ./build_machine.sh $i False False
     ./build_machine.sh $i False True
@@ -11,5 +12,18 @@ do
     # This next line to make sure building fails, if it isn't there.
     rm ../minion/constraints/generated_constraint_code.h
     cp machines/$i.False.False.vm_out ../minion/constraints/generated_constraint_code.h
-( cd ..; rm -rf bin-$i; mkdir bin-$i; cd bin-$i; cmake ..; make minion -j2; cp minion $initial_path/machines/minion-$i )
+    ( cd ..; rm -rf bin-$i; mkdir bin-$i; cd bin-$i; cmake ..; make minion -j2; cp minion $initial_path/machines/minion-$i-nosym )
+    cp machines/$i.False.True.vm_out ../minion/constraints/generated_constraint_code.h
+    ( cd ..; rm -rf bin-$i; mkdir bin-$i; cd bin-$i; cmake ..; make minion -j2; cp minion $initial_path/machines/minion-$i-sym )
 done
+
+for i in lifeImmigration lifeBriansBrain;
+do
+    ./build_machine.sh $i False True
+    ./build_machine.sh $i True True
+    # This next line to make sure building fails, if it isn't there.
+    rm ../minion/constraints/generated_constraint_code.h
+    cp machines/$i.False.True.vm_out ../minion/constraints/generated_constraint_code.h
+    ( cd ..; rm -rf bin-$i; mkdir bin-$i; cd bin-$i; cmake ..; make minion -j2; cp minion $initial_path/machines/minion-$i-sym )
+done
+
