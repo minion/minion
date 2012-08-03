7833f9179a8b913abab3937c839d9408f2a8e732
Peter Nightingale <pwn1@users.noreply.github.com>
2012-08-03 14:51:28 +0100

    update build all machines for smf exps

--- mini-scripts/build_all_machines.sh
+++ mini-scripts/build_all_machines.sh
@@ -5,10 +5,10 @@ initial_path=`pwd`
 # life3d 
 for i in labs_two labs_three labs_four life pegsol and_constraint;
 do
-    ./build_machine.sh $i False False
-    ./build_machine.sh $i False True
-    ./build_machine.sh $i True False
-    ./build_machine.sh $i True True
+    ./build_machine.sh $i False False H 5
+    ./build_machine.sh $i False True H 5
+    ./build_machine.sh $i True False H 5
+    ./build_machine.sh $i True True H 5
     # This next line to make sure building fails, if it isn't there.
     rm ../minion/constraints/generated_constraint_code.h
     cp machines/$i.False.False.vm_out ../minion/constraints/generated_constraint_code.h
@@ -17,10 +17,13 @@ do
     ( cd ..; rm -rf bin-$i; mkdir bin-$i; cd bin-$i; cmake ..; make minion -j2; cp minion $initial_path/machines/minion-$i-sym )
 done
 
+# Can also do labs five, no symmetry as long as you don't compile it. 
+./build_machine.sh labs_five True False H 5
+
 for i in labs_five labs_six lifeImmigration lifeBriansBrain;
 do
-    ./build_machine.sh $i False True
-    ./build_machine.sh $i True True
+    ./build_machine.sh $i False True H 5
+    ./build_machine.sh $i True True H 5
     # This next line to make sure building fails, if it isn't there.
     rm ../minion/constraints/generated_constraint_code.h
     cp machines/$i.False.True.vm_out ../minion/constraints/generated_constraint_code.h
