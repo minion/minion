f611ca8e35d9c2620ce815976c1c73cd4dbb9fc8
Peter Nightingale <pwn1@users.noreply.github.com>
2012-08-08 13:22:37 +0100

    take out brian's brain compiled one.

--- mini-scripts/build_all_machines.sh
+++ mini-scripts/build_all_machines.sh
@@ -11,22 +11,27 @@ do
     ./build_machine.sh $i True True H 5
     # This next line to make sure building fails, if it isn't there.
     rm ../minion/constraints/generated_constraint_code.h
-    cp machines/$i.False.False.vm_out ../minion/constraints/generated_constraint_code.h
+    cp machines/$i.False.False.H.5.vm_out ../minion/constraints/generated_constraint_code.h
     ( cd ..; rm -rf bin-$i; mkdir bin-$i; cd bin-$i; cmake ..; make minion -j2; cp minion $initial_path/machines/minion-$i-nosym )
-    cp machines/$i.False.True.vm_out ../minion/constraints/generated_constraint_code.h
+    cp machines/$i.False.True.H.5.vm_out ../minion/constraints/generated_constraint_code.h
     ( cd ..; rm -rf bin-$i; mkdir bin-$i; cd bin-$i; cmake ..; make minion -j2; cp minion $initial_path/machines/minion-$i-sym )
 done
 
 # Can also do labs five, no symmetry as long as you don't compile it. 
 ./build_machine.sh labs_five True False H 5
 
-for i in labs_five labs_six lifeImmigration lifeBriansBrain;
+
+# Ones that only work with vm and symmetry
+./build_machine.sh lifeBriansBrain True True H 5
+
+
+for i in labs_five labs_six lifeImmigration;
 do
     ./build_machine.sh $i False True H 5
     ./build_machine.sh $i True True H 5
     # This next line to make sure building fails, if it isn't there.
     rm ../minion/constraints/generated_constraint_code.h
-    cp machines/$i.False.True.vm_out ../minion/constraints/generated_constraint_code.h
+    cp machines/$i.False.True.H.5.vm_out ../minion/constraints/generated_constraint_code.h
     ( cd ..; rm -rf bin-$i; mkdir bin-$i; cd bin-$i; cmake ..; make minion -j2; cp minion $initial_path/machines/minion-$i-sym )
 done
 
