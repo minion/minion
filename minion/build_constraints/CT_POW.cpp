2e297ab60465fd7196daea063658178f0cdf36ac
Christopher Jefferson <chris@bubblescope.net>
2013-04-03 17:45:44 +0100

    Add negative pow support

--- minion/build_constraints/CT_POW.cpp
+++ minion/build_constraints/CT_POW.cpp
@@ -6,6 +6,9 @@
 */
 
 #include "../constraints/constraint_pow.h"
+#include "../constraints/constraint_pow_check.h"
+#include "../constraints/forward_checking.h"
+
 
 template<typename V1, typename V2>
 inline AbstractConstraint*
@@ -13,6 +16,17 @@ BuildCT_POW(StateObj* stateObj, const V1& vars, const V2& var2, ConstraintBlob&)
 {
   D_ASSERT(vars.size() == 2);
   D_ASSERT(var2.size() == 1);
+  // vars1 is special to avoid 0^0
+  if(vars[0].getInitialMin() < 0 ||
+     vars[1].getInitialMin() <= 0 ||
+     var2[0].getInitialMin() < 0)
+  {
+    typedef PowConstraint_Check<typename V1::value_type, typename V1::value_type, typename V2::value_type, false> PowConC;
+    AbstractConstraint* pow=new CheckAssignConstraint<PowConC, false>(stateObj, PowConC(stateObj, vars[0], vars[1], var2[0]));
+    // Now wrap it in new FC thing. Horrible hackery.
+    return forwardCheckingCon(stateObj, pow);
+  }
+
   return new PowConstraint<typename V1::value_type, typename V1::value_type,
                            typename V2::value_type>(stateObj, vars[0], vars[1], var2[0]);
 }
---
+++ minion/constraints/constraint_pow_check.h
@@ -0,0 +1,81 @@
+/*
+* Minion http://minion.sourceforge.net
+* Copyright (C) 2006-09
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+
+
+#ifndef CONSTRAINT_POW_CHECK_H
+#define CONSTRAINT_POW_CHECK_H
+
+#include "../constraints/constraint_checkassign.h"
+#include <math.h>
+
+// Not a constraint -- just contains a checker.
+
+template<typename T1, typename T2, typename T3, bool undef_zero>
+class PowConstraint_Check
+{
+  StateObj* stateObj;
+public:
+  typedef typename common_var_type3<T1,T2,T3>::type var_common;
+  typedef minion_array<var_common, 3> var_type;
+private:
+   var_type vars;
+public:
+
+  PowConstraint_Check(StateObj* _stateObj, const T1& v1, const T2& v2, const T3& v3)
+  : stateObj(_stateObj)
+  {
+    vars[0] = v1; vars[1] = v2; vars[2] = v3;
+  }
+
+  string constraint_name() const
+  { if(undef_zero) return "pow_undefzero"; else return "pow"; }
+
+  CONSTRAINT_ARG_LIST3(vars[0], vars[1], vars[2])
+
+  var_type& get_vars()
+  { return vars; }
+
+  double my_pow(DomainInt x, DomainInt y)
+  { return pow(checked_cast<double>(x), checked_cast<double>(y));}
+
+  virtual bool check_assignment(DomainInt* v, SysInt v_size)
+  {
+    D_ASSERT(v_size == 3);
+    if(v[1] == 0)
+    {
+      if(v[0] == 0)
+        return false;
+      else
+        return v[2] == 1;
+    }
+    if(v[1] < 0)
+    {
+      return my_pow(v[2], -v[1]) == v[0];
+    }
+    else
+    {
+      return my_pow(v[0], v[1]) == v[2];
+    }
+  }
+};
+
+
+#endif
