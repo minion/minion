825a900638075c20c6cefbad3f1c362a8e044111
Christopher Jefferson <chris@bubblescope.net>
2013-06-18 17:16:12 +0100

    Fix printing large integer

--- minion/preprocess.cpp
+++ minion/preprocess.cpp
@@ -32,7 +32,7 @@ void PropogateCSP(StateObj* stateObj, PropagationLevel preprocessLevel, vector<A
   if(preprocessLevel == PropLevel_GAC)
     return;
   
-  BigInt lits = lit_count(vars);
+  DomainInt lits = lit_count(vars);
   bool bounds_check = ( (preprocessLevel == PropLevel_SACBounds) ||
                         (preprocessLevel == PropLevel_SSACBounds) );
   
