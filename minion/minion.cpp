36f5b61eda1ffca2d37b847334aef21bd9119d80
Christopher Jefferson <chris@bubblescope.net>
2014-01-10 13:06:09 +0000

    Remove incorrect information about printing what constraints are compiled in

--- minion/minion.cpp
+++ minion/minion.cpp
@@ -70,8 +70,6 @@ void print_default_help(char** argv)
 #endif
   cout << "The following preprocessor flags were active:" << endl;
   print_macros();
-  cout << "The following constraints were compiled in:" << endl;
-  print_constraints();
 }
 
 void worker()
