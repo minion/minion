927ac119711ec0bc3786fe7d94a9f7501f42e862
Christopher Jefferson <chris@bubblescope.net>
2014-05-02 21:07:06 +0100

    Reduce size of special reified constraints

--- minion/build_constraints/CT_EQ_REIFY.cpp
+++ minion/build_constraints/CT_EQ_REIFY.cpp
@@ -1,17 +1,24 @@
 #include "../minion.h"
 /* Minion Constraint Solver
    http://minion.sourceforge.net
-   
-   For Licence Information see file LICENSE.txt 
+
+   For Licence Information see file LICENSE.txt
 */
 
 #include "../constraints/reify.h"
 #include "../constraints/constraint_equal.h"
 
+template<typename EqualVarRef1, typename BoolVarRef>
+AbstractConstraint*
+BuildCT_EQ_REIFY(StateObj* stateObj, const vector<EqualVarRef1>& var1,
+                                    const vector<EqualVarRef1>& var2, const vector<BoolVarRef> var3,ConstraintBlob&)
+{ return new ReifiedEqualConstraint<EqualVarRef1, EqualVarRef1, BoolVarRef>(stateObj,var1[0],var2[0],var3[0]); }
+
 template<typename EqualVarRef1, typename EqualVarRef2, typename BoolVarRef>
 AbstractConstraint*
-BuildCT_EQ_REIFY(StateObj* stateObj, const vector<EqualVarRef1>& var1, 
+BuildCT_EQ_REIFY(StateObj* stateObj, const vector<EqualVarRef1>& var1,
                                     const vector<EqualVarRef2>& var2, const vector<BoolVarRef> var3,ConstraintBlob&)
-{ return new ReifiedEqualConstraint<EqualVarRef1, EqualVarRef2, BoolVarRef>(stateObj,var1[0],var2[0],var3[0]); }
+{ return new ReifiedEqualConstraint<AnyVarRef, AnyVarRef, BoolVarRef>(stateObj,AnyVarRef(var1[0]),AnyVarRef(var2[0]),var3[0]); }
+
 
 BUILD_CT(CT_EQ_REIFY, 3)
