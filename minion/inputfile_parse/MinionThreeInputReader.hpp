aaa2deee9ce2a1b33efecba0db41e260f51a2a1c
Christopher Jefferson <chris@bubblescope.net>
2014-06-27 16:43:39 +0100

    Return deleted comment

--- minion/inputfile_parse/MinionThreeInputReader.hpp
+++ minion/inputfile_parse/MinionThreeInputReader.hpp
@@ -17,6 +17,370 @@
 * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+/** @help input Description
+
+Minion expects to be provided with the name of an input file as an
+argument. This file contains a specification of the CSP to be solved
+as well as settings that the search process should use. The format is
+
+Minion3Input::= MINION 3
+                <InputSection>+
+                **EOF**
+
+InputSection::= <VariablesSection>
+              | <SearchSection>
+              | <ConstraintsSection>
+              | <TuplelistSection>
+              | <ShortTuplelistSection>
+
+i.e. 'MINION 3' followed by any number of variable, search,
+constraints and tuplelists sections (can repeat) followed by
+'**EOF**', the end of file marker.
+
+All text from a '#' character to the end of the line is ignored.
+
+See the associated help entries below for information on each section.
+*/
+
+/** @help input Notes
+You can give an input file via standard input by specifying '--' as the file
+name, this might help when minion is being used as a tool in a shell script or
+for compressed input, e.g.,
+
+   gunzip -c myinput.minion.gz | minion
+*/
+
+/** @help input;variables Description
+The variables section consists of any number of variable declarations
+on separate lines.
+
+VariablesSection::= **VARIABLES**
+                    <VarDeclaration>*
+*/
+
+/** @help input;variables Example
+ **VARIABLES**
+
+BOOL bool                          #boolean var
+BOUND b {1..3}                     #bounds var
+SPARSEBOUND myvar {1,3,4,6,7,9,11} #sparse bounds var
+DISCRETE d[3] {1..3}               #array of discrete vars
+*/
+
+/** @help input;variables References
+See the help section
+
+   help variables
+
+for detailed information on variable declarations.
+*/
+
+/** @help input;constraints Description
+
+The constraints section consists of any number of constraint
+declarations on separate lines.
+
+ConstraintsSection::= **CONSTRAINTS**
+                      <ConstraintDeclaration>*
+*/
+
+/** @help input;constraints Example
+**CONSTRAINTS**
+eq(bool,0)
+alldiff(d)
+*/
+
+/** @help input;constraints References
+See help entries for individual constraints under
+
+   help constraints
+
+for details on constraint declarations.
+*/
+
+/** @help input;tuplelist Description
+A tuplelist section lists of allowed tuples for table constraints
+can be specified. This technique is preferable to specifying the
+tuples in the constraint declaration, since the tuplelists can be
+shared between constraints and named for readability.
+
+The required format is
+
+TuplelistSection::= **TUPLELIST**
+                    <Tuplelist>*
+
+Tuplelist::= <name> <num_tuples> <tuple_length> <numbers>+
+*/
+
+/** @help input;tuplelist Example
+**TUPLELIST**
+AtMostOne 4 3
+0 0 0
+0 0 1
+0 1 0
+1 0 0
+*/
+
+/** @help input;tuplelist References
+help constraints table
+help input shorttuplelist
+*/
+
+/** @help input;shorttuplelist Description
+A shorttuplelist section lists of allowed tuples for haggisgac
+and other constraints which accept short tuple lists.
+
+The required format is
+
+TuplelistSection::= **TUPLELIST**
+                    <Tuplelist>*
+
+Tuplelist::= <name> <num_tuples> <short_tuple>+
+
+short_tuple ::= [ <literal>*, ]
+
+literal ::= (<num>, <num>)
+*/
+
+/** @help input;shorttuplelist Example
+**SHORTTUPLELIST**
+mycon 4
+[(0,0),(3,0)]
+[(1,0),(3,0)]
+[(2,0),(3,0)]
+[(0,1),(1,1),(2,1),(3,1)]
+
+Represents the same constraint as:
+
+**TUPLELIST**
+mycon 8 4
+0 0 0 0
+0 0 1 0
+0 1 0 0
+0 1 1 0
+1 0 0 0
+1 0 1 0
+1 1 0 0
+1 1 1 1
+
+Short tuples give us a way of shrinking this list. Short tuples consist
+of pairs (x,y), where x is a varible position, and y is a value for that
+variable. For example:
+
+[(0,0),(3,0)]
+
+Represents 'If the variable at index 0 is 0, and the variable at index
+3 is 0, then the constraint is true'.
+
+
+Note that some tuples are double-represented in the example 'mycon'.
+The first 3 short tuples all allow the assignment '0 0 0 0'. This is fine.
+The important thing for efficency is to try to give a small list of
+short tuples.
+*/
+
+/** @help input;shorttuplelist References
+help constraints haggisgac
+help constraints haggisgac-stable
+help input tuplelist
+*/
+
+
+
+/** @help input;search Description
+
+Inside the search section one can specify
+
+- variable orderings,
+- value orderings,
+- optimisation function, and
+- details of how to print out solutions.
+
+   SearchSection::= <VarValOrdering>*
+                    <OptimisationFn>?
+                    <PrintFormat>?
+
+If no varval ordering is given then the variables are assigned in instantiation
+order and the values tried in ascending order.
+
+If a variable order is given as a command line argument it will override
+anything specified in the input file.
+
+Multiple variable orders can be given, each with an optional value ordering:
+
+   VarValOrdering::= <VarOrder>
+                     <ValOrder>?
+
+In each VarOrder an instantiation order is specified for a subset of
+variables. Variables can optionally be \"auxiliary variables\" (add \"AUX\" to
+the varorder) meaning that if there are several solutions to the problem
+differing only in the auxiliary variables, only one is reported by minion.
+
+   VarOrder::= VARORDER AUX? <ORDER>? [ <varname>+ ]
+
+      where
+
+   <ORDER>::= STATIC | SDF | SRF | LDF | ORIGINAL | WDEG | CONFLICT | DOMOVERWDEG
+
+The value ordering allows the user to specify an instantiation order
+for the variables involved in the variable order, either ascending (a)
+or descending (d) for each. When no value ordering is specified, the
+default is to use ascending order for every search variable.
+
+   ValOrder::= VALORDER[ (a|d)+ ]
+
+To model an optimisation problem the user can specify to minimise
+or maximise a variable's value.
+
+   OptimisationFn::= MAXIMISING <varname>
+                   | MINIMISING <varname>
+
+Finally, the user can control some aspects of the way solutions are
+printed. By default (no PrintFormat specified) all the variables are
+printed in declaration order. Alternatively a custom vector, or ALL
+variables, or no (NONE) variables can be printed. If a matrix or, more
+generally, a tensor is given instead of a vector, it is automatically
+flattened into a vector as described in 'help variables vectors'.
+
+   PrintFormat::= PRINT <vector>
+                | PRINT ALL
+                | PRINT NONE
+*/
+
+/** @help input;search References
+See also
+
+   switches -varorder
+*/
+
+/** @help input;example Example
+Below is a complete minion input file with commentary, as an example.
+
+MINION 3
+
+# While the variable section doesn't have to come first, you can't
+# really do anything until
+# You have one...
+**VARIABLES**
+
+# There are 4 type of variables
+BOOL bool         # Boolean don't need a domain
+BOUND b {1..3}    # Bound vars need a domain given as a range
+DISCRETE d {1..3} # So do discrete vars
+
+#Note: Names are case sensitive!
+
+# Internally, Bound variables are stored only as a lower and upper bound
+# Whereas discrete variables allow any sub-domain
+
+SPARSEBOUND s {1,3,6,7} # Sparse bound variables take a sorted list of values
+
+# We can also declare matrices of variables!
+
+DISCRETE q[3] {0..5} # This is a matrix with 3 variables: q[0],q[1] and q[2]
+BOOL bm[2,2] # A 2d matrix, variables bm[0,0], bm[0,1], bm[1,0], bm[1,1]
+BOOL bn[2,2,2,2] # You can have as many indices as you like!
+
+#The search section is entirely optional
+**SEARCH**
+
+# Note that everything in SEARCH is optional, and can only be given at
+# most once!
+
+# If you don't give an explicit variable ordering, one is generated.
+# These can take matrices in interesting ways like constraints, see below.
+VARORDER [bool,b,d]
+
+# If you don't give a value ordering, 'ascending' is used
+#VALORDER [a,a,a,a]
+
+# You can have one objective function, or none at all.
+MAXIMISING bool
+# MINIMISING x3
+
+# both (MAX/MIN)IMISING and (MAX/MIN)IMIZING are accepted...
+
+
+# Print statement takes a vector of things to print
+
+PRINT [bool, q]
+
+# You can also give:
+# PRINT ALL (the default)
+# PRINT NONE
+
+
+# Declare constraints in this section!
+**CONSTRAINTS**
+
+# Constraints are defined in exactly the same way as in MINION input
+formats 1 & 2
+eq(bool, 0)
+eq(b,d)
+
+# To get a single variable from a matrix, just index it
+eq(q[1],0)
+eq(bn[0,1,1,1], bm[1,1])
+
+# It's easy to get a row or column from a matrix. Just use _ in the
+# indices you want
+# to vary. Just giving a matrix gives all the variables in that matrix.
+
+#The following shows how flattening occurs...
+
+# [bm] == [ bm[_,_] ] == [ bm[0,0], bm[0,1], bm[1,0], bm[1,1] ]
+# [ bm[_,1] ] = [ bm[0,1], bm[1,1] ]
+# [ bn[1,_,0,_] = [ bn[1,0,0,0], b[1,0,0,1], b[1,1,0,0], b[1,1,0,1] ]
+
+# You can string together a list of such expressions!
+
+lexleq( [bn[1,_,0,_], bool, q[0]] , [b, bm, d] )
+
+# One minor problem.. you must always put [ ] around any matrix expression, so
+# lexleq(bm, bm) is invalid
+
+lexleq( [bm], [bm] ) # This is OK!
+
+# Can give tuplelists, which can have names!
+# The input is: <name> <num_of_tuples> <tuple_length> <numbers...>
+# The formatting can be about anything..
+
+**TUPLELIST**
+
+Fred 3 3
+0 2 3
+2 0 3
+3 1 3
+
+Bob 2 2 1 2 3 4
+
+#No need to put everything in one section! All sections can be reopened..
+**VARIABLES**
+
+# You can even have empty sections.. if you want
+
+**CONSTRAINTS**
+
+#Specify tables by their names..
+
+table([q], Fred)
+
+# Can still list tuples explicitally in the constraint if you want at
+# the moment.
+# On the other hand, I might remove this altogether, as it's worse than giving
+# Tuplelists
+
+table([q],{ <0,2,3>,<2,0,3>,<3,1,3> })
+
+#Must end with the **EOF** marker!
+
+**EOF**
+
+Any text down here is ignored, so you can write whatever you like (or
+nothing at all...)
+*/
+
+
 // Plan here is to generate an instance of a problem (or whatever you have)
 // and return that.
 
