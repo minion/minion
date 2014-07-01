9db9e369b718dab4cf5ab9c734a3e8fcc96d2138
Christopher Jefferson <chris@bubblescope.net>
2014-07-01 10:38:10 +0100

    Update docs for 1.7

--- minion/minion.h
+++ minion/minion.h
@@ -33,7 +33,7 @@
 //#ifdef WATCHEDLITERALS
 //#define DYNAMICTRIGGERS
 //#endif
-#define VERSION "Minion Version 1.6.1"
+#define VERSION "Minion Version 1.7"
 #define REVISION "HG revision $Revision$"
 // above line will work but only gives revision of this file,
 //  not the current global revision
