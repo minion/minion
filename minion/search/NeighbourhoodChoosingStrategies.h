21a5d81cfb496f9fc496a004816e190dd91394f4
saad_attieh <saad_attieh@me.com>
2017-03-05 10:48:05 +0000

    Added fake hasher to unordered_set<AnyVarRef> to allow compiling, NeighbourhoodSearchManager now takes choosing strategy  as a shared-ptr instead of reference refactorring and moving includes within ifdef guards

--- minion/search/NeighbourhoodChoosingStrategies.h
+++ minion/search/NeighbourhoodChoosingStrategies.h
@@ -1,45 +1,23 @@
-#include "neighbourhood-def.h"
-#include <vector>
-//
-// Created by Patrick Spracklen on 3/1/17.
-//
-
 #ifndef MINION_NEIGHBOURHOODCHOOSINGSTRATEGIES_H
 #define MINION_NEIGHBOURHOODCHOOSINGSTRATEGIES_H
+#include "neighbourhood-def.h"
+#include <vector>
 
-struct NeighbourhoodStats{
+struct NeighbourhoodStats {
   int minValue;
   u_int64_t timeTaken;
 };
 
+class RandomNeighbourhoodChooser {
+public:
+  void updateNeighbourhoodStats(vector<int>& activatedNeighbourhoods,
+                                NeighbourhoodStats& neighbourhoodStats) {}
 
-
-
-
-
-
-
-
-class RandomNeighbourhoodChooser{
-
-  void updateNeighbourhoodStats(vector<int> &activatedNeighbourhoods, NeighbourhoodStats &neighbourhoodStats){
-
-  }
-
-  vector<int> getNeighbourHoodsToActivate(const NeighbourhoodContainer &neighbourhoodContainer){
-    int random_variable = static_cast<int>(std::rand() % neighbourhoodContainer.neighbourhoods.size());
+  vector<int> getNeighbourHoodsToActivate(const NeighbourhoodContainer& neighbourhoodContainer) {
+    int random_variable =
+        static_cast<int>(std::rand() % neighbourhoodContainer.neighbourhoods.size());
     return vector<int>{random_variable};
   }
-
-
-
-
-
-
-
 };
 
-
-
-
-#endif //MINION_NEIGHBOURHOODCHOOSINGSTRATEGIES_H
+#endif // MINION_NEIGHBOURHOODCHOOSINGSTRATEGIES_H
--- minion/search/neighbourhood-search.h
+++ minion/search/neighbourhood-search.h
@@ -1,12 +1,11 @@
-#include "neighbourhood-def.h"
-#include <ctime>
-#include <cstdlib>
-#include <memory>
+
+#ifndef MINION_NEIGHBOURHOOD_SEARCH_H
+#define MINION_NEIGHBOURHOOD_SEARCH_H
 #include "NeighbourhoodChoosingStrategies.h"
-
-
-#ifndef MINION_NEIGHBOURHOOD_SEARCH
-#define MINION_NEIGHBOURHOOD_SEARCH
+#include "neighbourhood-def.h"
+#include <cstdlib>
+#include <ctime>
+#include <memory>
 
 void inline get_single_solution() {
   if(getState().isOptimisationProblem()) {
@@ -33,161 +32,149 @@ void inline get_single_solution() {
     throw EndOfSearch();
 }
 
-template<typename NeighbourhoodSelectionStrategy>
+template <typename NeighbourhoodSelectionStrategy>
 struct NeighbourhoodSearchManager : public Controller::SearchManager {
-    shared_ptr<Propagate> prop;
-    vector<SearchOrder> base_order;
-    NeighbourhoodContainer nhc;
-    const NeighbourhoodSelectionStrategy &selectionStrategy;
-    NeighbourhoodSearchManager(shared_ptr<Propagate> _prop,
-                               vector<SearchOrder> _base_order,
-                               NeighbourhoodContainer _nhc,
-                                const NeighbourhoodSelectionStrategy &selectionStrategy)
-    : prop(_prop), base_order(_base_order), nhc(_nhc), selectionStrategy(selectionStrategy)
-    { }
+  shared_ptr<Propagate> prop;
+  vector<SearchOrder> base_order;
+  NeighbourhoodContainer nhc;
+  shared_ptr<NeighbourhoodSelectionStrategy> selectionStrategy;
+  NeighbourhoodSearchManager(shared_ptr<Propagate> _prop, vector<SearchOrder> _base_order,
+                             NeighbourhoodContainer _nhc,
+                             shared_ptr<NeighbourhoodSelectionStrategy> selectionStrategy)
+      : prop(std::move(_prop)),
+        base_order(_base_order),
+        nhc(_nhc),
+        selectionStrategy(std::move(selectionStrategy)) {}
 
-    void searchNeighbourhoods(vector<DomainInt> &solution, DomainInt &minValue, vector<int> &activatedNeighbourhoods)
-    {
-        // Save state of the world
-        int depth = Controller::get_world_depth();
-        Controller::world_push();
+  void searchNeighbourhoods(vector<DomainInt>& solution, DomainInt& minValue,
+                            vector<int>& activatedNeighbourhoods) {
+    // Save state of the world
+    int depth = Controller::get_world_depth();
+    Controller::world_push();
 
-        auto vo = Controller::make_search_order_multiple(base_order);
+    auto vo = Controller::make_search_order_multiple(base_order);
 
-        if (activatedNeighbourhoods.empty()){
-            nhc.shadow_disable.assign(1);
-        } else{
-            switchOnNeighbourhoods(activatedNeighbourhoods);
-        }
-
-        prop->prop(vo->getVars());
-        if(getState().isFailed())
-        {
-            D_FATAL_ERROR("Neighbourhoods Assignment unsatisfiable");
-        }
-
-        auto sm = make_shared<Controller::StandardSearchManager>
-                    (vo, prop, Controller::standard_time_ctrlc_checks,
-                    [&solution, &minValue, this](){
-                        for(const auto& var : this->nhc.shadow_mapping[0])
-                            solution.push_back(var.getAssignedValue());
-                        minValue = getState().getOptimiseVar()->getMin();
-                        throw EndOfSearch();
-                    }, [](){});
-        try {
-            sm->search();
-        }
-        catch(EndOfSearch&)
-        { }
-        Controller::world_pop_to_depth(depth);
+    if(activatedNeighbourhoods.empty()) {
+      nhc.shadow_disable.assign(1);
+    } else {
+      switchOnNeighbourhoods(activatedNeighbourhoods);
     }
 
+    prop->prop(vo->getVars());
+    if(getState().isFailed()) {
+      D_FATAL_ERROR("Neighbourhoods Assignment unsatisfiable");
+    }
 
+    auto sm = make_shared<Controller::StandardSearchManager>(
+        vo, prop, Controller::standard_time_ctrlc_checks,
+        [&solution, &minValue, this]() {
+          for(const auto& var : this->nhc.shadow_mapping[0])
+            solution.push_back(var.getAssignedValue());
+          minValue = getState().getOptimiseVar()->getMin();
+          throw EndOfSearch();
+        },
+        []() {});
+    try {
+      sm->search();
+    } catch(EndOfSearch&) {}
+    Controller::world_pop_to_depth(depth);
+  }
 
-    virtual void search() 
-    {
-        vector<DomainInt> solution;
-        DomainInt  minValue;
-        searchNeighbourhoods(solution, minValue, {});
+  virtual void search() {
+    vector<DomainInt> solution;
+    DomainInt minValue;
+    vector<int> activatedNeighbourhoods;
 
-        if (solution.empty()){
-            return;
-        }
+    searchNeighbourhoods(solution, minValue, activatedNeighbourhoods);
 
+    if(solution.empty()) {
+      return;
+    }
 
+    copyOverIncumbent(solution);
+    while(
+        !(activatedNeighbourhoods = selectionStrategy->getNeighbourHoodsToActivate(nhc)).empty()) {
+      // Set the lower bound of the optimized variable
+      getState().getOptimiseVar()->setMin(minValue);
+      auto startTime = std::chrono::high_resolution_clock::now();
+      searchNeighbourhoods(solution, minValue, activatedNeighbourhoods);
+      auto endTime = std::chrono::high_resolution_clock::now();
+      u_int64_t timeTaken = getTimeTaken(startTime, endTime);
+      static_cast<void>(timeTaken); // temp remove warning of unused var
+
+      if(!solution.empty()) {
         copyOverIncumbent(solution);
-        vector<int> activatedNeighbourhoods;
-        while (!(activatedNeighbourhoods = selectionStrategy.getNeighbourHoodsToActivate(nhc)).empty()){
-            //Set the lower bound of the optimized variable
-            getState().getOptimiseVar()->setMin(minValue);
-            auto startTime = std::chrono::high_resolution_clock::now();
-            searchNeighbourhoods(solution, minValue, activatedNeighbourhoods);
-            auto endTime = std::chrono::high_resolution_clock::now();
-
-            if (!solution.empty()){
-                copyOverIncumbent(solution);
-
-                /*
-                 * 1. Need to update the neighbourhoods with the stats
-                 * 2.
-                 */
-
-
-
-            }
-
-        }
-
-        exit(1);
-    }
-
-    typedef std::chrono::high_resolution_clock::time_point timePoint;
-
-
-    u_int64_t getTimeTaken(timePoint startTime, timePoint endTime){
-      return std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
-    }
-
-
-
-
-
-
-
-    void updateNeighbourhoodStats(vector<int> &activatedNeighbourhoods, struct NeighbourhoodStats &neighbourhoodStats ){
-        selectionStrategy.updateNeighbourhoodStats(activatedNeighbourhoods, neighbourhoodStats);
-    }
-
-
-
-
-    /**
-     * Switch on the neighbourhood activation vars
-     * Find the set of primary variables not contained in any neighbourhoods and assign them to the incumbent solution
-     * @param neighbourHoodIndexes
-     */
-    void switchOnNeighbourhoods(const vector<int> &neighbourHoodIndexes){
-       std::unordered_set<AnyVarRef> shadowVariables();
-       for (int i: neighbourHoodIndexes){
-           Neighbourhood &neighbourhood = nhc.neighbourhoods[i];
-           neighbourhood.activation.assign(1);
-
-           for (auto & n:neighbourhood.vars){
-                shadowVariables().insert(n);
-           }
-       }
 
         /*
-         * If the primary variable is not contained in any shadow neighbourhoods, assign its incumbent solution
+         * 1. Need to update the neighbourhoods with the stats
+         * 2.
          */
-        for (auto &var: nhc.shadow_mapping[0]){
-           if (shadowVariables().count(var) == 0){
-              var.assign(var.getAssignedValue());
-           }
-        }
+      }
     }
 
-    void copyOverIncumbent(const vector<DomainInt> &solution){
-        for (int i = 0; i < nhc.shadow_mapping[0].size(); i++){
-           nhc.shadow_mapping[1][i].assign(nhc.shadow_mapping[0][i].getAssignedValue());
-        }
+    exit(1);
+  }
+
+  typedef std::chrono::high_resolution_clock::time_point timePoint;
+
+  u_int64_t getTimeTaken(timePoint startTime, timePoint endTime) {
+    return std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
+  }
+
+  void updateNeighbourhoodStats(vector<int>& activatedNeighbourhoods,
+                                struct NeighbourhoodStats& neighbourhoodStats) {
+    selectionStrategy->updateNeighbourhoodStats(activatedNeighbourhoods, neighbourhoodStats);
+  }
+
+  /**
+   * Switch on the neighbourhood activation vars
+   * Find the set of primary variables not contained in any neighbourhoods and assign them to the
+   * incumbent solution
+   * @param neighbourHoodIndexes
+   */
+  void switchOnNeighbourhoods(const vector<int>& neighbourHoodIndexes) {
+    struct FakeHasherAndEquals {
+      size_t operator()(const AnyVarRef&) const {
+        abort();
+      }
+      bool operator()(const AnyVarRef&, const AnyVarRef&) const {
+        abort();
+      }
+    };
+    std::unordered_set<AnyVarRef, FakeHasherAndEquals, FakeHasherAndEquals> shadowVariables;
+    for(int i : neighbourHoodIndexes) {
+      Neighbourhood& neighbourhood = nhc.neighbourhoods[i];
+      neighbourhood.activation.assign(1);
+
+      for(auto& n : neighbourhood.vars) {
+        shadowVariables.insert(n);
+      }
     }
 
+    /*
+     * If the primary variable is not contained in any shadow neighbourhoods, assign its incumbent
+     * solution
+     */
+    for(auto& var : nhc.shadow_mapping[0]) {
+      if(shadowVariables.count(var) == 0) {
+        var.assign(var.getAssignedValue());
+      }
+    }
+  }
 
-
+  void copyOverIncumbent(const vector<DomainInt>& solution) {
+    for(int i = 0; i < nhc.shadow_mapping[0].size(); i++) {
+      nhc.shadow_mapping[1][i].assign(nhc.shadow_mapping[0][i].getAssignedValue());
+    }
+  }
 };
 
-
-
-
-shared_ptr<Controller::SearchManager>
-MakeNeighbourhoodSearch(PropagationLevel prop_method,
-                        vector<SearchOrder> base_order,
-                        NeighbourhoodContainer nhc)
-{
-    shared_ptr<Propagate> prop = Controller::make_propagator(prop_method);
-    return std::make_shared<NeighbourhoodSearchManager>(prop, base_order, nhc);
+shared_ptr<Controller::SearchManager> MakeNeighbourhoodSearch(PropagationLevel prop_method,
+                                                              vector<SearchOrder> base_order,
+                                                              NeighbourhoodContainer nhc) {
+  shared_ptr<Propagate> prop = Controller::make_propagator(prop_method);
+  return std::make_shared<NeighbourhoodSearchManager<RandomNeighbourhoodChooser>>(
+      prop, base_order, nhc, std::make_shared<RandomNeighbourhoodChooser>());
 }
 
 #endif
-
