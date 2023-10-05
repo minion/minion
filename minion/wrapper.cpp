6fa49cd442fc5d3042ecf70cb451be2d3d3ac812
Niklas Dewally <nd60@st-andrews.ac.uk>
2023-10-05 13:31:32 +0100

    create wrapper.cpp and wrapper.h

---
+++ minion/wrapper.cpp
@@ -0,0 +1,11 @@
+// Minion https://github.com/minion/minion
+// SPDX-License-Identifier: MPL-2.0
+
+/*
+ * Functions for using minion as a library.
+ */
+
+#include <minion.h>
+#include <wrapper.h>
+
+void resetMinion() {}
---
+++ minion/wrapper.h
@@ -0,0 +1,20 @@
+// Minion https://github.com/minion/minion
+// SPDX-License-Identifier: MPL-2.0
+
+/*
+ * Functions for using minion as a library.
+ */
+
+#ifndef _WRAPPER_H
+#define _WRAPPER_H
+
+/*
+ * void resetMinion:
+ *
+ *  Reset all global variables in minion to their initial values.
+ *
+ */
+void resetMinion();
+
+
+#endif
