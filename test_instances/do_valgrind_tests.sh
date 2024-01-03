57b434e92c64b00562ed57aa09e450c329151598
Niklas Dewally <niklas@dewally.com>
2024-01-03 15:49:57 +0000

    do_basic_tests.sh: add #TEST EXITCODE1 mode to allow checking of whether minion deliberately fails or segfaults.

--- test_instances/do_valgrind_tests.sh
+++ test_instances/do_valgrind_tests.sh
@@ -4,7 +4,11 @@ set -o errexit
 
 if which valgrind; then
   for i in *.minion; do
-    if ! grep '#FAIL' $i > /dev/null; then
+    if grep '#FAIL' $i > /dev/null; then
+      :
+    elif grep '#TEST EXITCODE1' $i > /dev/null; then
+      :
+    else
       valgrind --error-exitcode=1 --leak-check=full $* $i -nodelimit 10
     fi
   done
